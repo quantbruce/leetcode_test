4cf4044fa32807b2855f31eca07e3bcf9e9efa93
quantbruce <48423247+quantbruce@users.noreply.github.com>
2019-10-07 11:16:43 +0800

    Create 069_Sqrt(x).py

---
+++ Algorithm-Easy/069_Sqrt(x).py
@@ -0,0 +1,13 @@
+#### Michelle Way
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 1, x//2 # right没必要从最后一位取
+        while left <= right:
+            mid = left + (right-left)//2
+            if mid <= x/mid:  # 这个=很重要，体会这种两边逼近的思想
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right # left-1也行  最后返回的时候left和right要么相遇，要么交叉
