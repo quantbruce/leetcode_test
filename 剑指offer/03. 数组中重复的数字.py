4aa6e1c6b8e4a06efc3948cf5a88f7701638f351
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-20 15:34:39 +0800

    Update 03. 数组中重复的数字.py

--- 剑指offer/03. 数组中重复的数字.py
+++ 剑指offer/03. 数组中重复的数字.py
@@ -1,1 +1,10 @@
+# 排序思路
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        pre = nums[0]
+        for i in range(1, len(nums)):
+            if pre == nums[i]:
+                return pre
+            pre = nums[i]
 
