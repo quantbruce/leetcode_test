7757b42d7c4f30c428cfb3c0f449c48defd4e93e
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-25 11:43:46 +0800

    Create 10- I. 斐波那契数列.py

---
+++ 剑指offer/10- I. 斐波那契数列.py
@@ -0,0 +1,8 @@
+https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/solution/mian-shi-ti-10-i-fei-bo-na-qi-shu-lie-dong-tai-gui/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b       # 这种写法能保证a+b时，a的值还未更新。注意区别分两行写的效果
+        return a % 1000000007
