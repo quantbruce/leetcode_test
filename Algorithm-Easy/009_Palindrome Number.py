0f54d7c20598cb295c3b31808963fb365f3ce9ed
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-25 10:46:56 +0800

    Update 009_Palindrome Number.py

--- Algorithm-Easy/009_Palindrome Number.py
+++ Algorithm-Easy/009_Palindrome Number.py
@@ -42,3 +42,29 @@ class Solution:
 
 if __name__ == '__main__':
     print(Solution().isPalindrome(0))
+
+
+#########简洁高效法    
+class Solution:
+def isPalindrome(self, x: int) -> bool:
+    return str(x)==str(x)[::-1]
+
+# 方法4: 将int转化成str类型: 双指针 (指针的性能一直都挺高的)
+# 复杂度: O(n)
+def isPalindrome(x: int) -> bool:
+    lst = list(str(x))
+    L, R = 0, len(lst)-1
+    while L <= R:
+        if lst[L] != lst[R]:
+            return  False
+        L += 1
+        R -= 1
+    return True
+
+######方法5
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return True if str(x)==str(x)[::-1] else False
+
+
+
