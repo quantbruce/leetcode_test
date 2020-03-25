c247f6e7519856cc7ef36b686a7f21269b78f813
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-25 15:12:13 +0800

    Create 11. 旋转数组的最小数字.py

---
+++ 剑指offer/11. 旋转数组的最小数字.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minArray(self, numbers: [int]) -> int:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            m = (i + j) // 2
+            if numbers[m] > numbers[j]: i = m + 1
+            elif numbers[m] < numbers[j]: j = m
+            else: j -= 1
+        return numbers[i]
+
+
+
+https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/mian-shi-ti-11-xuan-zhuan-shu-zu-de-zui-xiao-shu-3/
+
