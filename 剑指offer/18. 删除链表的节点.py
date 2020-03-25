34e0e34a2eb08661afa42ae4f2541a34321c2d93
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-25 17:25:36 +0800

    Create 18. 删除链表的节点.py

---
+++ 剑指offer/18. 删除链表的节点.py
@@ -0,0 +1,11 @@
+class Solution:
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+        if head.val == val: return head.next
+        pre, cur = head, head.next
+        while cur and cur.val != val:
+            pre, cur = cur, cur.next
+        pre.next = cur.next
+        return head
+
+
+https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/solution/mian-shi-ti-18-shan-chu-lian-biao-de-jie-dian-sh-2/
