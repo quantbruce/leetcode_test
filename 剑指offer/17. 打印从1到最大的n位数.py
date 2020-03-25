0325ff6af047497ba562b698efcf5a7f2ebd3485
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-25 16:32:18 +0800

    Create 17. 打印从1到最大的n位数.py

---
+++ 剑指offer/17. 打印从1到最大的n位数.py
@@ -0,0 +1,5 @@
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        return [i for i in range(1,10**n)]
+
+######################################这一题真是考察要点python容易掩盖，还需要继续斟酌！######################################
