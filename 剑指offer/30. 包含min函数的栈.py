37b7e07ebee92fdb651b8e065859ef0418344a50
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-26 23:26:38 +0800

    Create 30. 包含min函数的栈.py

---
+++ 剑指offer/30. 包含min函数的栈.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.A, self.B = [], []
+
+
+    def push(self, x: int) -> None:
+        self.A.append(x)
+        if not self.B or x<=self.B[-1]:
+            self.B.append(x)
+
+    def pop(self) -> None:
+        if self.A.pop() == self.B[-1]:
+            self.B.pop()
+
+    def top(self) -> int:
+        return self.A[-1]
+
+    def min(self) -> int:
+        return self.B[-1]
+        
+ 
+   https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/solution/mian-shi-ti-30-bao-han-minhan-shu-de-zhan-fu-zhu-z/
+       
