7cfa93de04794120ccd46923ff121c2135317a18
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-27 20:14:58 +0800

    Create 面试题32 - II. 从上到下打印二叉树 II.py

---
+++ 剑指offer/面试题32 - II. 从上到下打印二叉树 II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):  # 每一层的元素有多少个，就循环多少次。这里是精华
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(tmp)
+        return res
+        
+        
+        https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/solution/mian-shi-ti-32-ii-cong-shang-dao-xia-da-yin-er-c-5/
