2d3f4cd1bd3f742d4693526ff071f391af1440b1
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-28 19:15:24 +0800

    Update 53 - I. 在排序数组中查找数字 I.py

--- 剑指offer/53 - I. 在排序数组中查找数字 I.py
+++ 剑指offer/53 - I. 在排序数组中查找数字 I.py
@@ -29,9 +29,31 @@ class Solution:
         
         return right - left - 1
 
+    
+ ###########更展开详细的写法
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] < target: i = m + 1
+            elif nums[m] == target: i = m + 1 # 向右缩，因此是在找右边界
+            else: j = m - 1
+        right = i
+
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] < target: i = m + 1
+            elif nums[m] == target: j = m - 1 # 向左缩，因此是在找左边界
+            else: j = m - 1
+        left = j
+        
+        return right - left - 1
 
 链接：https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/solution/mian-shi-ti-53-i-zai-pai-xu-shu-zu-zhong-cha-zha-5/
 
+    
         
   
   
