2d6933062c308fa36a25e919e2887ee0058e395e
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-28 19:25:07 +0800

    Create 53 - II. 0～n-1中缺失的数字.py

---
+++ 剑指offer/53 - II. 0～n-1中缺失的数字.py
@@ -0,0 +1,12 @@
+#####有序数组---采用二分法
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] == m: i = m + 1
+            else: j = m - 1
+        return i
+
+https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/solution/mian-shi-ti-53-ii-0n-1zhong-que-shi-de-shu-zi-er-f/
