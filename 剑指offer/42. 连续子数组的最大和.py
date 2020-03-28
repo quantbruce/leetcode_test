ec2394c4598629fd179d51ae7adb72fc82d5a186
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-28 12:46:23 +0800

    Create 42. 连续子数组的最大和.py

---
+++ 剑指offer/42. 连续子数组的最大和.py
@@ -0,0 +1,10 @@
+##动态规划法
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] += max(nums[i-1], 0)
+        return max(nums)
+
+https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution/mian-shi-ti-42-lian-xu-zi-shu-zu-de-zui-da-he-do-2/
+
