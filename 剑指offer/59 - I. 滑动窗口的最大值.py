178d05e91d4ea08498d536904cc07c340be3096b
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-30 22:17:22 +0800

    Update 59 - I. 滑动窗口的最大值.py

--- 剑指offer/59 - I. 滑动窗口的最大值.py
+++ 剑指offer/59 - I. 滑动窗口的最大值.py
@@ -21,7 +21,29 @@ class Solution:
         return res
 
 
+    
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        deque=[]
+        result=[]
+        for i in range(len(nums)):
+            # 只存有可能成为最大值数字的index进入deque
+            while deque and nums[i] > nums[deque[-1]]:
+                deque.pop()
+            deque.append(i)
 
+            #如果相聚超过窗口长度k，丢弃掉deque中首位index
+            while i-deque[0]>k-1:
+                deque.pop(0)
+            # 只要满足一个窗口的长度k，就向result中添加nums[deque[0]一个结果
+            if i>=k-1:
+                result.append(nums[deque[0]]) #在整个过程中，始终保持deque[0]为最大值的index
+        return result
+    
+代码：    
+https://blog.csdn.net/weixin_43455338/article/details/105011557
+动画：
+https://www.cxyxiaowu.com/1141.html
 
 
 
