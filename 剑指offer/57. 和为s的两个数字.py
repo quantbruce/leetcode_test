4f3cc5b44f542baed7ec720ed8f5663d2d4cfab3
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-03-30 12:10:30 +0800

    Update 57. 和为s的两个数字.py

--- 剑指offer/57. 和为s的两个数字.py
+++ 剑指offer/57. 和为s的两个数字.py
@@ -31,3 +31,33 @@ class Solution:
 
 
 链接：https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/solution/mian-shi-ti-57-he-wei-s-de-liang-ge-shu-zi-shuang-/
+
+    
+    
+######滑动窗口法(最优)
+def findContinuousSequence(self, target: int) -> List[List[int]]:
+    i = 1 # 滑动窗口的左边界
+    j = 1 # 滑动窗口的右边界
+    sum = 0 # 滑动窗口中数字的和
+    res = []
+
+    while i <= target // 2:
+        if sum < target:
+            # 右边界向右移动
+            sum += j
+            j += 1
+        elif sum > target:
+            # 左边界向右移动
+            sum -= i
+            i += 1
+        else:
+            # 记录结果
+            arr = list(range(i, j))
+            res.append(arr)
+            # 左边界向右移动
+            sum -= i
+            i += 1
+
+    return res
+
+https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/solution/shi-yao-shi-hua-dong-chuang-kou-yi-ji-ru-he-yong-h/
