9072d5cb409e436bfcea4cf4012287eaa492c701
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-02 19:46:29 +0800

    Create 20. 表示数值的字符串.py

---
+++ 剑指offer/20. 表示数值的字符串.py
@@ -0,0 +1,16 @@
+##############感觉这题是很浪费时间的SB题，暂且只学会取巧法
+
+class Solution(object):
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        try:
+            float(s)
+            return True
+        except :
+            return False
+
+
+https://leetcode-cn.com/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof/solution/onpythonjie-ti-wu-fa-luo-ji-pan-duan-regexdfadeng-/
