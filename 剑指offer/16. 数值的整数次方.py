8d9c11d6376cc6d4f248880e43a27f734032cf3e
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-02 17:36:20 +0800

    Create 16. 数值的整数次方.py

---
+++ 剑指offer/16. 数值的整数次方.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0: return 0
+        res = 1
+        if n < 0: x, n = 1 / x, -n
+        while n:
+            if n & 1: res *= x
+            x *= x
+            n >>= 1
+        return res
+
+
+https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/solution/mian-shi-ti-16-shu-zhi-de-zheng-shu-ci-fang-kuai-s/
