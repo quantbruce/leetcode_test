ca96502139369fa72b0338d26ee951d3688446a3
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-03 11:09:40 +0800

    Create 32 - III. 从上到下打印二叉树 III.py

---
+++ 剑指offer/32 - III. 从上到下打印二叉树 III.py
@@ -0,0 +1,17 @@
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(tmp[::-1] if len(res) % 2 else tmp)
+        return res
+
+
+https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/solution/mian-shi-ti-32-iii-cong-shang-dao-xia-da-yin-er--3/
