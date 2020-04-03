d6b52c4339bedfe05ea394ed32ed9a6591b46990
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-03 11:46:20 +0800

    Update 32 - III. 从上到下打印二叉树 III.py

--- 剑指offer/32 - III. 从上到下打印二叉树 III.py
+++ 剑指offer/32 - III. 从上到下打印二叉树 III.py
@@ -1,3 +1,4 @@
+#######方法一最优
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root: return []
@@ -13,5 +14,37 @@ class Solution:
             res.append(tmp[::-1] if len(res) % 2 else tmp)
         return res
 
+ 
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, deque = [], collections.deque()
+        deque.append(root)
+        while deque:
+            tmp = []
+            # 打印奇数层
+            for _ in range(len(deque)):
+                # 从左向右打印
+                node = deque.popleft()
+                tmp.append(node.val)
+                # 先左后右加入下层节点
+                if node.left: deque.append(node.left)
+                if node.right: deque.append(node.right)
+            res.append(tmp)
+            if not deque: break
+            # 打印偶数层
+            tmp = []
+            for _ in range(len(deque)):
+                # 从右向左打印
+                node = deque.pop()
+                tmp.append(node.val)
+                # 先右后左加入下层节点
+                if node.right: deque.appendleft(node.right)
+                if node.left: deque.appendleft(node.left)
+            res.append(tmp)
+        return res
+ 
+    
 
 https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/solution/mian-shi-ti-32-iii-cong-shang-dao-xia-da-yin-er--3/
