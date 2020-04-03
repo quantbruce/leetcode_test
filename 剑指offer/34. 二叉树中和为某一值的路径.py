49fb841f01ad907ffac205ef8f634ac9a0cb57b6
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-03 17:46:37 +0800

    Create 34. 二叉树中和为某一值的路径.py

---
+++ 剑指offer/34. 二叉树中和为某一值的路径.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        res, path = [], []
+        def recur(root, tar):
+            if not root: return
+            path.append(root.val)
+            tar -= root.val
+            if tar == 0 and not root.left and not root.right:
+                res.append(list(path))
+            recur(root.left, tar)
+            recur(root.right, tar)
+            path.pop()
+
+        recur(root, sum)
+        return res
+
+
+https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/solution/mian-shi-ti-34-er-cha-shu-zhong-he-wei-mou-yi-zh-5/
