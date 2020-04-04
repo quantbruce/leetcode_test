954aa5284853736117d484944019992fb0c6e9d2
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-04 11:58:31 +0800

    Create 35. 复杂链表的复制.py

---
+++ 剑指offer/35. 复杂链表的复制.py
@@ -0,0 +1,78 @@
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        return copy.deepcopy(head)
+
+
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        def dfs(head):
+            if not head: return None
+            if head in visited:
+                return visited[head]
+            # 创建新结点
+            copy = Node(head.val, None, None)
+            visited[head] = copy
+            copy.next = dfs(head.next)
+            copy.random = dfs(head.random)
+            return copy
+        visited = {}
+        return dfs(head)
+
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        visited = {}
+    
+        def bfs(head):
+            if not head: return head
+            clone = Node(head.val, None, None) # 创建新结点
+            queue = collections.deque()
+            queue.append(head)
+            visited[head] = clone
+            while queue:
+                tmp = queue.pop()
+                if tmp.next and tmp.next not in visited:
+                    visited[tmp.next] = Node(tmp.next.val, [], [])
+                    queue.append(tmp.next)  
+                if tmp.random and tmp.random not in visited:
+                    visited[tmp.random] = Node(tmp.random.val, [], [])
+                    queue.append(tmp.random)
+                visited[tmp].next = visited.get(tmp.next)
+                visited[tmp].random = visited.get(tmp.random)
+            return clone  ####################################??????????????????
+        return bfs(head)
+
+
+
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        visited = {}
+        def getClonedNode(node):
+            if node:
+                if node in visited:
+                    return visited[node]
+                else:
+                    visited[node] = Node(node.val,None,None)
+                    return visited[node]
+            return None
+        
+        if not head: return head
+        old_node = head
+        new_node = Node(old_node.val,None,None)
+        visited[old_node] = new_node
+
+        while old_node:
+            new_node.random = getClonedNode(old_node.random)
+            new_node.next = getClonedNode(old_node.next)
+            
+            old_node = old_node.next
+            new_node = new_node.next
+        return visited[head]
+
+
+https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof/solution/lian-biao-de-shen-kao-bei-by-z1m/
+
+
+
