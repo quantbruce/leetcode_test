3e5bc5e8b21f265338bb8934da39de68b28b09a5
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-19 17:35:03 +0800

    Create 48. 最长不含重复字符的子字符串.py

---
+++ 剑指offer/48. 最长不含重复字符的子字符串.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s)<2: return len(s)
+        res = 1
+        head = tail = 0
+
+        while tail<len(s)-1: # 因为下面马上就要tail+=1, 所以不能取等号，否则会发生越界。
+            tail+=1
+            if s[tail] not in s[head:tail]:
+                res = max(tail-head+1, res)  # 因为实际向右滑动的过程中，窗口很可能会缩短。
+            else:                            # 为了确保res的数最大，要始终确保其递增。
+                while s[tail] in s[head:tail]:  # 因为窗口中相等的元素若不在左端点，则需要移动多次，所以需要while循环
+                    head += 1
+        return res
