09396ab9633533b73c1df5577c2e446918173927
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-19 16:31:52 +0800

    Create 47. 礼物的最大价值.py

---
+++ 剑指offer/47. 礼物的最大价值.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j == 0: continue
+                if i == 0: grid[i][j] += grid[i][j - 1]
+                elif j == 0: grid[i][j] += grid[i - 1][j]
+                else: grid[i][j] += max(grid[i][j - 1], grid[i - 1][j])
+        return grid[-1][-1]
+
+
+"""
+执行用时 :
+52 ms
+, 在所有 Python3 提交中击败了
+91.16%
+的用户
+内存消耗 :
+14.9 MB
+, 在所有 Python3 提交中击败了
+100.00%
+的用户
+"""
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        for j in range(1, len(grid[0])): # 初始化第一行
+            grid[0][j] += grid[0][j - 1]
+        for i in range(1, len(grid)): # 初始化第一列
+            grid[i][0] += grid[i - 1][0]
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += max(grid[i][j - 1], grid[i - 1][j])
+        return grid[-1][-1]
+
+https://leetcode-cn.com/problems/li-wu-de-zui-da-jie-zhi-lcof/solution/mian-shi-ti-47-li-wu-de-zui-da-jie-zhi-dong-tai-gu/
+
