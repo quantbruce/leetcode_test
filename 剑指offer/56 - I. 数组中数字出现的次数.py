398fc59b510c2c06f326cbe7d70948e82b1c1dfd
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-23 19:57:48 +0800

    Update 56 - I. 数组中数字出现的次数.py

--- 剑指offer/56 - I. 数组中数字出现的次数.py
+++ 剑指offer/56 - I. 数组中数字出现的次数.py
@@ -13,6 +13,17 @@
 的用户
 """
 
+首先想到字典的key都是唯一的(字典特性)，便可已利用key记录nums中独立元素，而对应的value则作为该key的出现次数，然后分成两个阶段解题：
+1）首先是通过遍历nums列表的所有元素，若元素字典中没有，则将其导入字典dic作为key, value赋值1; 若字典中已经出现过该元素，则直接在之前value上 +1即可；
+2）通过遍历1）处理完毕的字典dic, 将统计次数value为1的key, 添加到结果数组res中，即完成解答。
+
+不足：空间复杂度过高, 暂时还没想到其他好办法。抛砖引玉, 欢迎提供优化建议。
+
+作者：quantbruce
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/solution/jie-jin-shuang-100-zi-dian-si-lu-jian-dan-yi-li-ji/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
 class Solution:
     def singleNumbers(self, nums: List[int]) -> List[int]:
         dic = {}
@@ -28,4 +39,16 @@ class Solution:
                 res.append(key)
         return res
 
+    
+其实可以在第一次遇见的时候在字典建立key和value，第二次遇见的时候直接删掉（因为重复数字只会出现两次），这样遍历完列表之后直接获取字典中剩下的key就可以了。我也是这么做的，不过似乎用字典就没办法达到题目要求的空间O(1)了。
+
+class Solution:
+    def singleNumbers(self, nums: List[int]) -> List[int]:
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                del dic[num]
+        return list(dic.keys())
 
