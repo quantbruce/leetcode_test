51fc3495a59d44a39b566cd0962ed6f7f3fe7ea3
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-04-23 22:56:54 +0800

    Update 04. 二维数组中的查找.py

--- 剑指offer/04. 二维数组中的查找.py
+++ 剑指offer/04. 二维数组中的查找.py
@@ -5,7 +5,7 @@ https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/solution/m
 class Solution:
     def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
         i, j= len(matrix)-1,0
-        while i>=0 and j< len(matrix[0]):
+        while i>=0 and j< len(matrix[0]):  #j不取等号是由于，如果i和j都取了等号，则就没有留给任何空间给target了，因为matrix[i][j]能取到最右上角元素
             if target < matrix[i][j]:
                 i -= 1
             elif target > matrix[i][j]:
