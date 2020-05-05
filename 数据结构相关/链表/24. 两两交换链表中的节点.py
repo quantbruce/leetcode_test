809aa9ea09e0ec9a04192e50021d06507f5d2504
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-05-05 11:53:12 +0800

    Create 24. 两两交换链表中的节点.py

---
+++ 数据结构相关/链表/24. 两两交换链表中的节点.py
@@ -0,0 +1,121 @@
+24. 两两交换链表中的节点
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+
+
+"""
+执行用时 :
+44 ms
+, 在所有 Python3 提交中击败了
+43.85%
+的用户
+内存消耗 :
+13.8 MB
+, 在所有 Python3 提交中击败了
+6.25%
+的用户
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#############方法1(递归法)
+class Solution(object):
+    def swapPairs(self, head: ListNode) -> ListNode:
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        # If the list has no node or has only one node left.
+        if not head or not head.next:
+            return head
+
+        # Nodes to be swapped
+        first_node = head
+        second_node = head.next
+
+        # Swapping
+        first_node.next  = self.swapPairs(second_node.next)
+        second_node.next = first_node
+
+        # Now the head is the second node
+        return second_node
+
+
+https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-19/
+
+
+#################方法2(迭代法、while循环) 
+##体会：这个稍微好理解写，空间复杂度也更低，但是节点间关系错综复杂一些，要多练习屡清思路
+
+"""
+执行用时 :
+36 ms
+, 在所有 Python3 提交中击败了
+80.01%
+的用户
+内存消耗 :
+13.8 MB
+, 在所有 Python3 提交中击败了
+6.25%
+的用户
+"""
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Dummy node acts as the prevNode for the head node
+        # of the list and hence stores pointer to the head node.
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        prev_node = dummy
+
+        while head and head.next:
+
+            # Nodes to be swapped
+            first_node = head;
+            second_node = head.next;
+
+            # Swapping
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            # Reinitializing the head and prev_node for next swap
+            prev_node = first_node
+            head = first_node.next
+
+        # Return the new head node.
+        return dummy.next
+
+
+https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-19/
+
+
+
+
+
+
+
+
+
+
+
+
