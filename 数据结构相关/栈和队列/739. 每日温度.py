ab7329e201e825b5974ef2c59be16654f99182ae
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-06 09:57:33 +0800

    Update 739. 每日温度.py

--- 数据结构相关/栈和队列/739. 每日温度.py
+++ 数据结构相关/栈和队列/739. 每日温度.py
@@ -6,6 +6,10 @@
 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
 
 
+######################方法1，(递减堆栈)
+
+#tips: 认真看那个动画，草稿纸上多笔画。
+
 """
 执行用时 :
 508 ms
@@ -21,16 +25,22 @@
 
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
-        res = [0] * len(T)
-        stack = []
+        res = [0] * len(T)  # 这下为最后的元素为0买下伏笔
+        stack = []  # stack在这里主要是记录T[]中要比较元素的下标的
 
         for i in range(len(T)):
-            while stack and T[i] > T[stack[-1]]:
+            while stack and T[i] > T[stack[-1]]: # 如果stack不为空，且要进行比较的最新元素>之前栈stack的栈顶元素stack[-1]
                 # pop the smaller value's index
-                small = stack.pop()
-                res[small] = i - small
+                small = stack.pop()  # small是个满足题设要求的栈顶元素下标
+                res[small] = i - small # i - small是比较的次数，也是首次要大的元素相隔的距离
             
             # append the index of current value 
             stack.append(i)
 
         return res
+    
+    
+    https://leetcode-cn.com/problems/daily-temperatures/solution/leetcode-tu-jie-739mei-ri-wen-du-by-misterbooo/
+        
+        
+        
