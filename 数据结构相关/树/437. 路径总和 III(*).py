ce868859fc1c91a27b179810a22764cf0a818b95
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-15 23:35:49 +0800

    Update and rename 437. 路径总和 III.py to 437. 路径总和 III(*).py

--- 数据结构相关/树/437. 路径总和 III(*).py
+++ 数据结构相关/树/437. 路径总和 III(*).py
@@ -66,8 +66,33 @@ https://leetcode-cn.com/problems/path-sum-iii/solution/437lu-jing-zong-he-iii-di
 https://leetcode-cn.com/problems/path-sum-iii/solution/hot-100-437lu-jing-zong-he-iii-python3-li-jie-di-g/
 
 
+#########################方法二，前缀和法。最优
+#####tips: 细节还没完全吃透
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        prefixSumTree = {0:1}
+        self.count = 0
+        prefixSum = 0
+        self.dfs(root, sum, prefixSum, prefixSumTree)
+        return self.count      
+        
+    def dfs(self, root, sum, prefixSum, prefixSumTree):
+        if not root:
+            return 0
+        prefixSum += root.val
+        oldSum = prefixSum - sum
+        if oldSum in prefixSumTree:
+            self.count += prefixSumTree[oldSum]
+        prefixSumTree[prefixSum] = prefixSumTree.get(prefixSum, 0) + 1
+        self.dfs(root.left, sum, prefixSum, prefixSumTree)
+        self.dfs(root.right, sum, prefixSum, prefixSumTree)
+        
+        '''一定要注意在递归回到上一层的时候要把当前层的prefixSum的个数-1，类似回溯，要把条件重置'''
+        prefixSumTree[prefixSum] -= 1
 
 
+ https://leetcode-cn.com/problems/path-sum-iii/solution/hot-100-437lu-jing-zong-he-iii-python3-li-jie-di-g/
 
 
 
