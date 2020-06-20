865c2580a0a2bd649243a74f0f5a563cf28c55f6
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-20 17:42:07 +0800

    Create 637. 二叉树的层平均值.py

---
+++ 数据结构相关/树/637. 二叉树的层平均值.py
@@ -0,0 +1,68 @@
+637. 二叉树的层平均值
+给定一个非空二叉树, 返回一个由每层节点平均值组成的数组.
+
+示例 1:
+
+输入:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+输出: [3, 14.5, 11]
+解释:
+第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
+注意：
+
+节点值的范围在32位有符号整数范围内。
+
+
+######################方法1： BFS 层序遍历
+
+"""
+执行用时：
+60 ms
+, 在所有 Python3 提交中击败了
+81.84%
+的用户
+内存消耗：
+16.1 MB
+, 在所有 Python3 提交中击败了
+16.67%
+的用户
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root: return 0
+        res = []
+        queue = [root]
+
+        while queue:
+            cur = []
+            nxt = []
+            for node in queue:
+                cur.append(node.val)
+                if node.left:
+                    nxt.append(node.left) # 注意不是 node.left.val，nxt要与quque保持同样结构，内部都是装节点
+                if node.right:
+                    nxt.append(node.right)
+            res.append(sum(cur)/len(cur))
+            queue = nxt
+        return res
+        
+https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/solution/er-cha-shu-xi-lie-637-er-cha-shu-de-ceng-ping-jun-/
+
+
+
+
+
+        
+   
