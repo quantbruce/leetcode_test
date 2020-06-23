8e4288bc50874ba84f649be5e5af135563e39752
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-23 22:00:03 +0800

    Update 637. 二叉树的层平均值.py

--- 数据结构相关/树/637. 二叉树的层平均值.py
+++ 数据结构相关/树/637. 二叉树的层平均值.py
@@ -60,9 +60,44 @@ class Solution:
         
 https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/solution/er-cha-shu-xi-lie-637-er-cha-shu-de-ceng-ping-jun-/
 
+################BFS的令一种写法
 
-
-
+"""
+执行用时：
+60 ms
+, 在所有 Python3 提交中击败了
+82.02%
+的用户
+内存消耗：
+15.9 MB
+, 在所有 Python3 提交中击败了
+16.67%
+的用户
+"""
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root: return 0
+        queue = [root]
+        res = []
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if node:
+                    tmp.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            l = len(tmp)  # 统计这一层共有多少个节点，所以缩进的位置当然要放在这里
+            if l>0:
+                res.append(sum(tmp)/l)
+        return res
 
         
    
+
+
+
+
+
+
+
