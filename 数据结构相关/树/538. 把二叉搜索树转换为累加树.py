9e9de798b05c3d2ca55f401cdd9c87d27c51204d
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-26 22:03:41 +0800

    Create 538. 把二叉搜索树转换为累加树.py

---
+++ 数据结构相关/树/538. 把二叉搜索树转换为累加树.py
@@ -0,0 +1,51 @@
+538. 把二叉搜索树转换为累加树
+给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+
+例如：
+
+输入: 原始二叉搜索树:
+              5
+            /   \
+           2     13
+
+输出: 转换为累加树:
+             18
+            /   \
+          20     13
+          
+###########################方法1 递归回溯法
+
+"""
+执行用时：
+84 ms
+, 在所有 Python3 提交中击败了
+54.82%
+的用户
+内存消耗：
+15.8 MB
+, 在所有 Python3 提交中击败了
+16.67%
+的用户
+"""
+
+class Solution:
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        if root:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
+        
+        
+https://leetcode-cn.com/problems/convert-bst-to-greater-tree/solution/ba-er-cha-sou-suo-shu-zhuan-huan-wei-lei-jia-shu-3/
+
+
+
+
+
+
+        
