a142791a42fcd87d131f6e71a6708bc9a6c2ba1f
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-27 21:42:15 +0800

    Update 109. 有序链表转换二叉搜索树.py

--- 数据结构相关/树/109. 有序链表转换二叉搜索树.py
+++ 数据结构相关/树/109. 有序链表转换二叉搜索树.py
@@ -18,7 +18,8 @@
 
 ##########################方法1 （递归+快慢双指针法)
 #########直观思路，不做任何转化，这题的时间复杂度和空间复杂度分析值得细品
-#########仔细看题解的PPT，最好在稿纸上自己笔画下
+#########仔细看题解的PPT，最好在稿纸上自己笔画下,
+#########还有要注意的是，经实测代码发现，该题答案又不是唯一的，测试代码不通过，但是提交代码却能够accept
 
 """
 执行用时：
@@ -63,7 +64,38 @@ https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/solut
 
 ##########################方法2  转化数组法（典型空间换时间思路)
 
+"""
+执行用时：
+72 ms
+, 在所有 Python3 提交中击败了
+97.30%
+的用户
+内存消耗：
+20.1 MB
+, 在所有 Python3 提交中击败了
+14.29%
+的用户
+"""
 
+class Solution:
+    def mapListToValues(self, head):
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals
 
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        values = self.mapListToValues(head)
+        def convertListToBST(left, right):
+            if left > right: return None 
+            mid = (left + right) // 2
+            node = TreeNode(values[mid])
+            if left == right: return node      # 这行代码起到提前阻断的作用，可有可无，感觉实际时间并没有降低多少    
+            node.left = convertListToBST(left, mid-1)
+            node.right = convertListToBST(mid+1, right)
+            return node
+        return convertListToBST(0, len(values)-1) 
 
-
+      
+https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/solution/you-xu-lian-biao-zhuan-huan-er-cha-sou-suo-shu-by-/
