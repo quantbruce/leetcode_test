46db79fa12e1468664751beecb3e2a37ee04c306
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-27 17:02:32 +0800

    Update 236. 二叉树的最近公共祖先.py

--- 数据结构相关/树/236. 二叉树的最近公共祖先.py
+++ 数据结构相关/树/236. 二叉树的最近公共祖先.py
@@ -43,7 +43,7 @@ p、q 为不同节点且均存在于给定的二叉树中。
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root or root == p or root == q: return root
-        left = self.lowestCommonAncestor(root.left, p, q)
+        left = self.lowestCommonAncestor(root.left, p, q)  # left和right都是TreeNode对象，要理解
         right = self.lowestCommonAncestor(root.right, p, q)
         if not left and not right: return # 1.
         if not left: return right # 3.
