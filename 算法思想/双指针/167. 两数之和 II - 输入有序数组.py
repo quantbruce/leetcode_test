d2f822db99001f4473973122eb60a4ef8d16a927
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-30 19:55:05 +0800

    Update 167. 两数之和 II - 输入有序数组.py

--- 算法思想/双指针/167. 两数之和 II - 输入有序数组.py
+++ 算法思想/双指针/167. 两数之和 II - 输入有序数组.py
@@ -35,7 +35,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers)-1
-        while left < right:
+        while left < right:  # 双指针解法要下意识想到这个
             if numbers[left] + numbers[right] > target:
                 right -= 1
             elif numbers[left] + numbers[right] < target:
