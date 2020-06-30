d586eb40a08a987c407556b7040a74382810fb48
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-06-30 21:52:31 +0800

    Update 345. 反转字符串中的元音字母.py

--- 算法思想/双指针/345. 反转字符串中的元音字母.py
+++ 算法思想/双指针/345. 反转字符串中的元音字母.py
@@ -13,6 +13,9 @@
 元音字母不包含字母"y"。
 
 
+################方法1  目标元素入栈法
+### 体会下翻滚的意思，实际上就是利用了栈，单独对元音字母逆序操作
+
 """
 执行用时：
 60 ms
@@ -38,3 +41,42 @@ class Solution:
             else:
                 res.append(ls.pop())
         return ''.join(res)
+
+https://leetcode-cn.com/problems/reverse-vowels-of-a-string/solution/shuang-zhi-zhen-huo-zhe-lie-biao-de-popfang-fa-shi/
+    
+    
+############方法2 双指针法 (更优)
+###这两个算法的时间复杂度和空间复杂度要自己细扣下
+
+"""
+执行用时：
+56 ms
+, 在所有 Python3 提交中击败了
+92.50%
+的用户
+内存消耗：
+14.9 MB
+, 在所有 Python3 提交中击败了
+16.67%
+的用户
+"""
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        x = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+        res = []
+        j = len(s)-1
+
+        for i, k in enumerate(s):
+            if k in x:
+                while s[j] not in x:
+                    j-=1
+                res.append(s[j])
+                j-=1
+            else:
+                res.append(k)
+        return ''.join(res)
+    
+    
+    https://leetcode-cn.com/problems/reverse-vowels-of-a-string/solution/shuang-zhi-zhen-huo-zhe-lie-biao-de-popfang-fa-shi/
+    
