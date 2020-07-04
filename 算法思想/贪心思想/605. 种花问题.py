c07b38e088b05c5003e73bf0891985a85244a6de
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-04 15:19:33 +0800

    Create 605. 种花问题.py

---
+++ 算法思想/贪心思想/605. 种花问题.py
@@ -0,0 +1,48 @@
+605. 种花问题
+假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+
+给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+
+示例 1:
+
+输入: flowerbed = [1,0,0,0,1], n = 1
+输出: True
+示例 2:
+
+输入: flowerbed = [1,0,0,0,1], n = 2
+输出: False
+注意:
+
+数组内已种好的花不会违反种植规则。
+输入的数组长度范围为 [1, 20000]。
+n 是非负整数，且不会超过输入数组的大小。
+
+
+
+######## 方法1 左右区间补0法
+####该法很巧妙，效率也高
+
+"""
+执行用时：
+200 ms
+, 在所有 Python3 提交中击败了
+79.26%
+的用户
+内存消耗：
+14 MB
+, 在所有 Python3 提交中击败了
+10.00%
+的用户
+"""
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1]==0 and flowerbed[i]==0 and flowerbed[i+1]==0:  # 连续三个0则可以种一盆花
+                n-=1
+                flowerbed[i]=1
+        return n<=0
+        
+https://leetcode-cn.com/problems/can-place-flowers/solution/lian-xu-san-ge-0ze-ke-yi-chong-yi-pen-hua-by-ya-le/
+        
