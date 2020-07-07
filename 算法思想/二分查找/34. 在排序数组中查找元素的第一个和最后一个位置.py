d8b64b965efb0ea2642c120adc420c18b3729497
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-07 22:25:40 +0800

    Update 34. 在排序数组中查找元素的第一个和最后一个位置.py

--- 算法思想/二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.py
+++ 算法思想/二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -48,8 +48,47 @@ class Solution:
 https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-de-di-yi-/
 
 
+#############方法2 两次复用 二分查找
+######自己看了很久博客，时间花太多了，不过还是自己最后写的，效率太低不满意！
 
+"""
+执行用时：
+32 ms
+, 在所有 Python3 提交中击败了
+97.70%
+的用户
+内存消耗：
+14.4 MB
+, 在所有 Python3 提交中击败了
+7.69%
+的用户
+"""
 
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums: return [-1, -1]
+        res = []
+        # 找区间左端点
+        left1, right1 = 0, len(nums)-1
+        while left1 <= right1:
+            mid1 = (left1+right1)//2
+            if nums[mid1] < target: left1 = mid1 + 1
+            elif nums[mid1] > target: right1 = mid1 - 1
+            elif nums[mid1] == target: right1 = mid1 - 1
+        if left1 >= len(nums) or nums[left1] != target:
+            return [-1, -1]
+        res.append(left1)
+        # 找区间右端点
+        left2, right2 = 0, len(nums)-1
+        while left2 <= right2:
+            mid2 = (left2 + right2)//2
+            if nums[mid2] < target: left2 = mid2 + 1
+            elif nums[mid2] > target: right2 = mid2 - 1
+            elif nums[mid2] == target: left2 = mid2 + 1
+        if right2 < 0 or nums[right2] != target:
+            return [-1, -1]
+        res.append(right2)
+        return res
 
 https://labuladong.gitbook.io/algo/suan-fa-si-wei-xi-lie/er-fen-cha-zhao-xiang-jie
 
