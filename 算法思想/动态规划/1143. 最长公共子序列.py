ad0da77bd32e0c2f763359cbbdbf1096b31e201b
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-11 14:59:52 +0800

    Create 1143. 最长公共子序列.py

---
+++ 算法思想/动态规划/1143. 最长公共子序列.py
@@ -0,0 +1,70 @@
+1143. 最长公共子序列
+给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+
+若这两个字符串没有公共子序列，则返回 0。
+
+
+示例 1:
+
+输入：text1 = "abcde", text2 = "ace" 
+输出：3  
+解释：最长公共子序列是 "ace"，它的长度为 3。
+示例 2:
+
+输入：text1 = "abc", text2 = "abc"
+输出：3
+解释：最长公共子序列是 "abc"，它的长度为 3。
+示例 3:
+
+输入：text1 = "abc", text2 = "def"
+输出：0
+解释：两个字符串没有公共子序列，返回 0。
+ 
+
+提示:
+
+1 <= text1.length <= 1000
+1 <= text2.length <= 1000
+输入的字符串只含有小写英文字符。
+
+
+#############方法1  递归 (暴力法)
+### 理解原理即可
+
+"""
+超时
+"""
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        def dp(i, j):
+            if i == -1 or j == -1: return 0
+            if text1[i] == text2[j]:
+                return dp(i-1, j-1) + 1
+            else:
+                return max(dp(i-1, j), dp(i, j-1))
+        return dp(len(text1)-1, len(text2)-1)
+        
+https://labuladong.gitbook.io/algo/dong-tai-gui-hua-xi-lie/zui-chang-gong-gong-zi-xu-lie
+
+
+###############方法2 动态规划(二维)
+#######很经典的面试题，务必吃透掌握
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
+        return dp[-1][-1]
+
+#时间复杂度：O(N)
+#空间复杂度：O(1) ？？？？ 不确定
+
