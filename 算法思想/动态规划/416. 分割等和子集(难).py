6464efbaacb3cfc38357086a3e852f35f5d05d6c
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-12 22:05:03 +0800

    Update 416. 分割等和子集(难).py

--- 算法思想/动态规划/416. 分割等和子集(难).py
+++ 算法思想/动态规划/416. 分割等和子集(难).py
@@ -47,18 +47,18 @@ class Solution:
         if sum_ & 1 != 0: return False
         n = len(nums)
         sum_ = sum_ // 2
-        dp = [[False for _ in range(sum_+1)] for _ in range(n+1)]
-        for i in range(n+1):
+        dp = [[False for _ in range(sum_+1)] for _ in range(n+1)]  # base case 就是 dp[..][0] = true 和 dp[0][..] = false，
+        for i in range(n+1):                                       #   因为背包没有空间的时候，就相当于装满了，而当没有物品可选择的时候，肯定没办法装满背包。
             dp[i][0] = True # 恰好能将背包装满时，就是True. 要理解本题题意
         
         for i in range(1, n+1):
             for j in range(1, sum_+1):
                 if j - nums[i-1] < 0:
-                    dp[i][j] = dp[i-1][j]
+                    dp[i][j] = dp[i-1][j]  # 注意下标，这个地方老犯错
                 else:
                     dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
         return dp[n][sum_] # 改成dp[-1][-1]也是可行的
-        
+
 #时间复杂度：O(n*sum_)
 #空间复杂度：O(n*sum_)
 https://labuladong.gitbook.io/algo/di-ling-zhang-bi-du-xi-lie/bei-bao-zi-ji     
