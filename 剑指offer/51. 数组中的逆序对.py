3b595220ef22575b9013ca17904edb3950f9327c
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-15 23:45:05 +0800

    Create 51. 数组中的逆序对.py

---
+++ 剑指offer/51. 数组中的逆序对.py
@@ -0,0 +1,84 @@
+剑指 Offer 51. 数组中的逆序对
+在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+
+
+示例 1:
+
+输入: [7,5,6,4]
+输出: 5
+
+
+限制：
+
+0 <= 数组长度 <= 50000
+
+
+#############方法1  My Methods 线性扫描 暴力法
+"""
+超时
+"""
+
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]>nums[j]:
+                    count+=1
+        return count
+
+#时间复杂度: O(N^2)
+#空间复杂度：O(1)
+
+
+##############方法2 归并排序 官方
+
+"""
+执行用时：
+1652 ms
+, 在所有 Python3 提交中击败了
+66.37%
+的用户
+内存消耗：
+18.6 MB
+, 在所有 Python3 提交中击败了
+100.00%
+的用户
+"""
+
+class Solution:
+    def mergeSort(self, nums, tmp, l, r):
+        if l>=r: return 0
+        mid = (l+r)//2
+        inv_count = self.mergeSort(nums, tmp, l, mid) + self.mergeSort(nums, tmp, mid+1, r)
+        i, j, pos = l, mid+1, l
+        while i<=mid and j<=r:
+            if nums[i]<=nums[j]:               
+                tmp[pos] = nums[i]
+                i += 1
+                inv_count += (j-(mid+1))
+            else:
+                tmp[pos] = nums[j]
+                j+=1
+            pos+=1
+
+        for k in range(i,mid+1):  # 不是很理解接下来这两个for循环
+            tmp[pos] = nums[k]
+            inv_count += (j - (mid+1))
+            pos += 1
+
+        for k in range(j, r+1):
+            tmp[pos] = nums[k]
+            pos+=1
+        nums[l:r+1] = tmp[l:r+1]
+        return inv_count
+
+    def reversePairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        tmp = [0] * n
+        return self.mergeSort(nums, tmp, 0, n-1)
+
+
+https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/solution/shu-zu-zhong-de-ni-xu-dui-by-leetcode-solution/
+
+
