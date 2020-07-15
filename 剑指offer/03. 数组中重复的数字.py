6f2816c1c3af3b9773d5cbcba4519db9b59a4adb
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-16 00:02:44 +0800

    Update 03. 数组中重复的数字.py

--- 剑指offer/03. 数组中重复的数字.py
+++ 剑指offer/03. 数组中重复的数字.py
@@ -10,3 +10,34 @@ class Solution:
                 return pre
             pre = nums[i]
 
+            
+################方法2 My Methods 字典去重法
+
+"""
+执行用时：
+68 ms
+, 在所有 Python3 提交中击败了
+24.67%
+的用户
+内存消耗：
+28.9 MB
+, 在所有 Python3 提交中击败了
+100.00%
+的用
+"""
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        d = {}
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+
+        for k, val in enumerate(nums):
+            if d[val]!=1:
+                return val
+
+#时间复杂度：O(N) 
+#空间复杂度：O(N)
+   
+            
+            
+            
