6fbd30a927eb0692a00a8475176652343ddf8c3c
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-16 14:20:30 +0800

    Create 希尔排序.py

---
+++ 十大经典排序算法/希尔排序.py
@@ -0,0 +1,18 @@
+def ShellSort(nums):
+    gap = 1
+    while gap < len(nums)/3:
+        gap = gap * 3 + 1
+    while gap > 0:
+        for i in range(gap, len(nums)):
+            temp = nums[i]
+            j = i - gap
+            while j >= 0 and nums[j] > temp:
+                nums[j + gap] = nums[j]
+                j -= gap
+            nums[j + gap] = temp
+        gap = gap//3
+    return nums
+
+
+nums = [81, 94, 11, 96, 12, 35, 17, 95, 28, 58, 41, 75, 15]
+print(ShellSort(nums))
