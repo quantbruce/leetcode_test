460071e0b60cc724f7badf05069da31851108eac
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-16 12:56:56 +0800

    Create 插入排序.py

---
+++ 十大经典排序算法/插入排序.py
@@ -0,0 +1,13 @@
+
+def InsertionSort(nums):
+    for i in range(len(nums)):
+        preIndex = i - 1
+        current = nums[i]
+        while preIndex >= 0 and nums[preIndex] > current:
+            nums[preIndex+1] = nums[preIndex]
+            preIndex -= 1
+        nums[preIndex+1] = current
+    return nums
+
+nums = [6, 10, 43, 32, 23, 14]
+print(InsertionSort(nums))
