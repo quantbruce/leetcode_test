cb6d7e62d307b9110bcaa2ad0663411119054372
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-19 22:09:02 +0800

    Update 06. 从尾到头打印链表.py

--- 剑指offer/06. 从尾到头打印链表.py
+++ 剑指offer/06. 从尾到头打印链表.py
@@ -8,3 +8,8 @@ class Solution:
             stack.append(head.val)
             head = head.next
         return stack[::-1]
+
+#时间复杂度 O(N)： 入栈和出栈共使用 O(N)时间。
+#空间复杂度 O(N)： 辅助栈 stack 和数组 res 共使用O(N)的额外空间
+
+
