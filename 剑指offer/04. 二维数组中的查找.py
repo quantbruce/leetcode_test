5c9906d3574a017149896af7cf0229f848d24e41
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-19 21:50:44 +0800

    Update 04. 二维数组中的查找.py

--- 剑指offer/04. 二维数组中的查找.py
+++ 剑指offer/04. 二维数组中的查找.py
@@ -4,7 +4,7 @@ https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/solution/m
 
 class Solution:
     def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
-        i, j= len(matrix)-1,0
+        i, j= len(matrix)-1,0 #这里不用加 if not matrix: return False 因为，matrix为空不报错，但matrix为空时引用matrix[0]就会报错，此时while第一个条件就不满足，更轮不到后面报错了。
         while i>=0 and j< len(matrix[0]):  # 关于i,j的边界取值问题。因为 i 的取值范围是 [0, len(matrix) - 1] ， j 的取值范围是 [0, len(matrix[0]) - 1] ，可以对照一下~ 比如一个长度为 n 的数组，索引取值范围是 [0, n-1]
             if target < matrix[i][j]:
                 i -= 1
