288fcb84e07ab54d487b8bec7a56f456095a9a90
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-19 21:59:45 +0800

    Update 05. 替换空格.py

--- 剑指offer/05. 替换空格.py
+++ 剑指offer/05. 替换空格.py
@@ -9,5 +9,9 @@ class Solution:
                 list_.append(rep)
         return ''.join(list_)            #### .join()函数实现list->str
     
-    
- https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/solution/mian-shi-ti-05-ti-huan-kong-ge-ji-jian-qing-xi-tu-/
+时间复杂度 O(N)： 遍历使用 O(N) ，每轮添加（修改）字符操作使用 O(1)；
+空间复杂度 O(N)： Python 新建的 list 和 Java 新建的 StringBuilder 都使用了线性大小的额外空间。
+
+https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/solution/mian-shi-ti-05-ti-huan-kong-ge-ji-jian-qing-xi-tu-/
+
+        
