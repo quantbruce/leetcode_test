eb769ab4c16a231d4a1cf85470ce66c84f0c3c92
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-20 22:13:55 +0800

    Update 12. 矩阵中的路径.py

--- 剑指offer/12. 矩阵中的路径.py
+++ 剑指offer/12. 矩阵中的路径.py
@@ -3,10 +3,10 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(i, j, k):
-            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]: return False
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]: return False # 注意是or连接
             if k == len(word) - 1: return True
             tmp, board[i][j] = board[i][j], '/'
-            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1) # 这几项顺序可以打乱的
             board[i][j] = tmp
             return res
 
