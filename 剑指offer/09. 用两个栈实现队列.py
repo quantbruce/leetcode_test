479e757d25ef598db41d39162b40d18138f0fa61
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-20 20:07:15 +0800

    Update 09. 用两个栈实现队列.py

--- 剑指offer/09. 用两个栈实现队列.py
+++ 剑指offer/09. 用两个栈实现队列.py
@@ -12,5 +12,6 @@ class CQueue:
                 self.B.append(self.A.pop())
         return self.B.pop() if self.B else -1
     
-    
+    #时间复杂度: append() O(1), deletehead(), O(N)
+    #空间复杂度：最坏O(N)
     https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/solution/mian-shi-ti-09-yong-liang-ge-zhan-shi-xian-dui-l-2/
