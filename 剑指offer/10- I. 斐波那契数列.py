b16bacd4973341bb3554e6d2c05ced411c345042
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-20 20:31:00 +0800

    Update 10- I. 斐波那契数列.py

--- 剑指offer/10- I. 斐波那契数列.py
+++ 剑指offer/10- I. 斐波那契数列.py
@@ -1,4 +1,5 @@
 https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/solution/mian-shi-ti-10-i-fei-bo-na-qi-shu-lie-dong-tai-gui/
+# 由于 Python 中整形数字的大小限制 取决计算机的内存 （可理解为无限大），因此可不考虑大数越界问题。
 
 class Solution:
     def fib(self, n: int) -> int:
@@ -6,3 +7,7 @@ class Solution:
         for _ in range(n):
             a, b = b, a + b       # 这种写法能保证a+b时，a的值还未更新。注意区别分两行写的效果
         return a % 1000000007
+    
+#时间复杂度：O(N)
+#空间复杂度：O(1)
+
