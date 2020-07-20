3e3b899ec42f6f6a7f7706ae1768ad44e6c348a6
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-20 20:50:23 +0800

    Update 10- II. 青蛙跳台阶问题.py

--- 剑指offer/10- II. 青蛙跳台阶问题.py
+++ 剑指offer/10- II. 青蛙跳台阶问题.py
@@ -6,4 +6,18 @@ class Solution:
         for _ in range(n):
             a, b = b, a + b
         return a % 1000000007
+#时间复杂度：O(N)
+#空间复杂度：O(1)
 
+#########方法2 动态规划
+##写的不太对...给别人检查下问题
+class Solution:
+    def numWays(self, n: int) -> int:
+        dp = [1] * (n+2)
+        dp[0] = 1
+        dp[1] = 1
+        while n >=2:
+            dp[n] = dp[n-1] + dp[n-2]
+        return dp[n]%1000000007
+
+    
