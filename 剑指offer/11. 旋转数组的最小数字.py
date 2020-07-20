8de0c4eab72b111e8a139a947f1202df03e95e9a
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-20 21:19:25 +0800

    Update 11. 旋转数组的最小数字.py

--- 剑指offer/11. 旋转数组的最小数字.py
+++ 剑指offer/11. 旋转数组的最小数字.py
@@ -5,10 +5,10 @@ class Solution:
             m = (i + j) // 2
             if numbers[m] > numbers[j]: i = m + 1
             elif numbers[m] < numbers[j]: j = m
-            else: j -= 1
+            else: j -= 1  # 这一部的分情况讨论很见功底，多理解下
         return numbers[i]
 
-
-
+#时间复杂度：O(log(n))
+#空间复杂度：O(1)
 https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/mian-shi-ti-11-xuan-zhuan-shu-zu-de-zui-xiao-shu-3/
 
