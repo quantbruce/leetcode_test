4b756ee2258a2b2838156e72eb3c137025512543
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-21 09:49:17 +0800

    Update 14- II. 剪绳子 II.py

--- 剑指offer/14- II. 剪绳子 II.py
+++ 剑指offer/14- II. 剪绳子 II.py
@@ -21,7 +21,14 @@ class Solution:
         elif b==1: return 3**(a-1)*4 % 1000000007
         else: return 3**a*2 % 1000000007
 
+#时间复杂度：O(1)
+#空间复杂度：O(1)
 
+
+#############方法2 考虑大数越界
+"""
+Python 代码（第三栏）： 由于语言特性，理论上 Python 中的变量取值范围由系统内存大小决定（无限大），因此在 Python 中其实不用考虑大数越界问题。
+"""
 
 class Solution:
     def cuttingRope(self, n: int) -> int:
@@ -34,6 +41,8 @@ class Solution:
         if b == 0: return (rem * 3) % p # = 3^a % p
         if b == 1: return (rem * 4) % p # = 3^(a-1) * 4 % p
         return (rem * 6) % p # = 3^a * 2 % p
-
-
+    
+#时间复杂度：O(log(N))
+#空间复杂度：O(1)
 https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof/solution/mian-shi-ti-14-ii-jian-sheng-zi-iitan-xin-er-fen-f/
+
