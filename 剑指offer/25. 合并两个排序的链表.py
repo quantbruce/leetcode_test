4879f1af1e87659b59d7cb528c28a9ab78e68e68
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-22 17:29:46 +0800

    Update 25. 合并两个排序的链表.py

--- 剑指offer/25. 合并两个排序的链表.py
+++ 剑指offer/25. 合并两个排序的链表.py
@@ -1,3 +1,18 @@
+###############方法1 双指针+哑节点法
+###好好体会下 一开始设置哑节点的巧妙，全部在l1或l2上整合，不现实。
+
+"""
+执行用时：
+56 ms
+, 在所有 Python3 提交中击败了
+93.99%
+的用户
+内存消耗：
+14.4 MB
+, 在所有 Python3 提交中击败了
+100.00%
+的用户
+"""
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         cur = dum = ListNode(0)  # (1) dum = ListNode(0) 生成dum节点, （2）(指针)cur = dum(节点), cur指针指向dum节点。
@@ -9,5 +24,7 @@ class Solution:
             cur = cur.next
         cur.next = l1 if l1 else l2
         return dum.next
-        
-        https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/solution/mian-shi-ti-25-he-bing-liang-ge-pai-xu-de-lian-b-2/
+    
+#时间复杂度：O(M+N)
+#空间复杂度：O(1)
+https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/solution/mian-shi-ti-25-he-bing-liang-ge-pai-xu-de-lian-b-2/
