036b1d334548b74dd30f18cda66254fc934e2479
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-22 23:40:38 +0800

    Update 29. 顺时针打印矩阵.py

--- 剑指offer/29. 顺时针打印矩阵.py
+++ 剑指offer/29. 顺时针打印矩阵.py
@@ -5,16 +5,22 @@ class Solution:
         while True:
             for i in range(l, r+1): res.append(matrix[t][i])
             t += 1
-            if t>b: break
+            if t>b: break              # t>b错写成t<b,记住t的初始下标是0，越往下下标越大
             for i in range(t, b+1): res.append(matrix[i][r])
             r -= 1
             if l>r: break
-            for i in range(r, l-1, -1): res.append(matrix[b][i])
+            for i in range(r, l-1, -1): res.append(matrix[b][i]) # range(r, l-1, -1)忘记下标是一路递减的，漏写了
             b -= 1
             if t>b: break
             for i in range(b, t-1, -1): res.append(matrix[i][l])
             l += 1
             if l>r: break
         return res
-        
-        https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/solution/mian-shi-ti-29-shun-shi-zhen-da-yin-ju-zhen-she-di/
+    
+#时间复杂度: O(M*N)
+#空间复杂度: O(1)
+https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/solution/mian-shi-ti-29-shun-shi-zhen-da-yin-ju-zhen-she-di/
+
+    
+    
+    
