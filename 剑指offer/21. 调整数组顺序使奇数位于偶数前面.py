caafd8297d8a8a50810fe0a5efe25bd2cc0268d9
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-22 11:46:19 +0800

    Update 21. 调整数组顺序使奇数位于偶数前面.py

--- 剑指offer/21. 调整数组顺序使奇数位于偶数前面.py
+++ 剑指offer/21. 调整数组顺序使奇数位于偶数前面.py
@@ -13,3 +13,14 @@ class Solution:
 #时间复杂度：O(N)
 #空间复杂度：O(N)
 
+#############方法2 双指针法
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        i, j = 0, len(nums)-1
+        while i < j:
+            while i < j and nums[i]&1: i += 1
+            while i < j and nums[j]&1==0: j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        return nums
+#时间复杂度：O(N)
+#空间复杂度：O(1)
