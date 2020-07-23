6ca339aa348330b43ada599a8755980afcc94332
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-23 18:05:09 +0800

    Update 34. 二叉树中和为某一值的路径.py

--- 剑指offer/34. 二叉树中和为某一值的路径.py
+++ 剑指offer/34. 二叉树中和为某一值的路径.py
@@ -1,3 +1,5 @@
+###########方法1 递归法
+###有两个坑，易错点已经代码中进行注释
 """
 执行用时 :
 44 ms
@@ -19,13 +21,14 @@ class Solution:
             path.append(root.val)
             tar -= root.val
             if tar == 0 and not root.left and not root.right:
-                res.append(list(path))  
+                res.append(list(path))   #体会下为什么要加这个list()
             recur(root.left, tar)
             recur(root.right, tar)
-            path.pop()
+            path.pop()  # 结合题解PPT，在脑海中在现这行代码，体会其必要性
 
         recur(root, sum)
         return res
-
-
+    
+#时间复杂度：O(N)
+#空间复杂度：O(N)
 https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/solution/mian-shi-ti-34-er-cha-shu-zhong-he-wei-mou-yi-zh-5/
