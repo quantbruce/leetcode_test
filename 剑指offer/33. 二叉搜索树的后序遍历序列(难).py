f894c3a97eac8e090168adeea27306471dbb5a72
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-23 17:32:38 +0800

    Update and rename 33. 二叉搜索树的后序遍历序列.py to 33. 二叉搜索树的后序遍历序列(难).py

--- 剑指offer/33. 二叉搜索树的后序遍历序列(难).py
+++ 剑指offer/33. 二叉搜索树的后序遍历序列(难).py
@@ -1,15 +1,17 @@
-class Solution:
-    def verifyPostorder(self, postorder: [int]) -> bool:
+###################方法1 递归分治
+class Solution(object):
+    def verifyPostorder(self, postorder):
         def recur(i, j):
-            if i >= j: return True
-            l = i
-            while postorder[l] < postorder[j]: l += 1
-            m = l
-            while postorder[l] > postorder[j]: l += 1
-            return l == j and recur(i, m - 1) and recur(m, j - 1)
-
-        return recur(0, len(postorder) - 1)
-
+            if i>=j: return True
+            p = i
+            while postorder[p] < postorder[j]: p+=1
+            m = p
+            while postorder[p] > postorder[j]: p+=1
+            return p==j and recur(i, m-1) and recur(m, j-1)
+        return recur(0, len(postorder)-1)
+    
+#时间复杂度: O(N**2)
+#空间复杂度：O(N)
 
 ##########################################这种方法很费解，还没吃透。吃了一下午，后续反复肯！！！！
 class Solution:
