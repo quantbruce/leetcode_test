307888900f39218e29955e51f4a06f2bf827ad44
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-24 00:02:24 +0800

    Update 36. 二叉搜索树与双向链表(难).py

--- 剑指offer/36. 二叉搜索树与双向链表(难).py
+++ 剑指offer/36. 二叉搜索树与双向链表(难).py
@@ -11,6 +11,7 @@
 的用户
 """
 
+##################方法1 初始版
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
         def dfs(cur):
@@ -26,6 +27,47 @@ class Solution:
         head = head.right
         head.left, self.pre.right = self.pre, head
         return head
-
+    
+#时间复杂度：O(N)
+#空间复杂度：O(N)
 
 https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/solution/mian-shi-ti-36-er-cha-sou-suo-shu-yu-shuang-xian-5/
+
+    
+###################方法2 另写
+"""
+执行用时：
+52 ms
+, 在所有 Python3 提交中击败了
+35.87%
+的用户
+内存消耗：
+14.9 MB
+, 在所有 Python3 提交中击败了
+100.00%
+的用户
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def dfs(cur):
+            if not cur: return 
+            dfs(cur.left) # cur的移动通过这两个递归
+            if self.pre: # 这里未想到if-else这个分情况讨论，漏了
+                self.pre.right, cur.left = cur, self.pre # 一边遍历，一边调整双向指针指向
+            else:
+                self.head = cur # cur的初始赋值，这里我漏了
+            self.pre = cur # pre只能通过这条路平移迭代
+            dfs(cur.right)
+        
+        if not root: return
+        self.pre = None 
+        dfs(root)
+        self.head.left, self.pre.right = self.pre, self.head
+        return self.head
+    
+https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/solution/mian-shi-ti-36-er-cha-sou-suo-shu-yu-shuang-xian-5/
+#时间复杂度：O(N)
+#空间复杂度：O(N)
+    
+    
