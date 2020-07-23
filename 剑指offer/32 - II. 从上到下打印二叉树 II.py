2096e0223f5972967e3947645eec5daeb1f6e915
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-23 11:42:59 +0800

    Update 32 - II. 从上到下打印二叉树 II.py

--- 剑指offer/32 - II. 从上到下打印二叉树 II.py
+++ 剑指offer/32 - II. 从上到下打印二叉树 II.py
@@ -12,6 +12,8 @@ class Solution:
                 if node.right: queue.append(node.right)
             res.append(tmp)
         return res
-        
-        
-        https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/solution/mian-shi-ti-32-ii-cong-shang-dao-xia-da-yin-er-c-5/
+    
+#时间复杂度 O(N)： N为二叉树的节点数量，即BFS需循环N次。
+#空间复杂度 O(N)： 最差情况下，即当树为平衡二叉树时，最多有N/2个树节点同时在queue中，使用O(N)大小的额外空间。?????????????
+
+https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/solution/mian-shi-ti-32-ii-cong-shang-dao-xia-da-yin-er-c-5/
