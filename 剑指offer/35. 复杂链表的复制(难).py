bcdb1d13309e63c3c5e8b9018be98bbcad1f35dd
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-23 22:06:45 +0800

    Update and rename 35. 复杂链表的复制.py to 35. 复杂链表的复制(难).py

--- 剑指offer/35. 复杂链表的复制(难).py
+++ 剑指offer/35. 复杂链表的复制(难).py
@@ -1,25 +1,30 @@
+###############方法1 面试官骂人法
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         return copy.deepcopy(head)
 
 
-
+###############方法2 DFS
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         def dfs(head):
             if not head: return None
-            if head in visited:
+            if head in visited: # visited是用来保存已经生成的节点的，字典能代去重属性
                 return visited[head]
             # 创建新结点
             copy = Node(head.val, None, None)
-            visited[head] = copy
+            visited[head] = copy    # 这一行这种映射关系总容易弄错
             copy.next = dfs(head.next)
             copy.random = dfs(head.random)
             return copy
         visited = {}
         return dfs(head)
-
-
+    
+#时间复杂度: O(N)
+#空间复杂度: O(N)
+ 
+    
+####################方法3 BFS
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         visited = {}
@@ -40,12 +45,35 @@ class Solution:
                     queue.append(tmp.random)
                 visited[tmp].next = visited.get(tmp.next)
                 visited[tmp].random = visited.get(tmp.random)
-            return clone  ####################################??????????????????
+            return clone  ####################################??????????????????， 看清答主解释，clone是复制后的链表的头节点
+        return bfs(head)
+
+########我默写的方法3 
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        visited = {}
+        def bfs(head):
+            if not head: return head
+            clone = Node(head.val, None, None)
+            queue = [head]
+            visited[head] = clone
+            while queue:
+                tmp = queue.pop(0) # pop(0)也能AC, 感觉这样更符合队列的味道
+                if tmp.next and tmp.next not in visited:
+                    visited[tmp.next] = Node(tmp.next.val, None, None)
+                    queue.append(tmp.next)
+                if tmp.random and tmp.random not in visited:
+                    visited[tmp.random] = Node(tmp.random.val, None, None)
+                    queue.append(tmp.random)
+                visited[tmp].next = visited.get(tmp.next) #奇怪，为什么visited[tmp.next]就会报错，非空报错？
+                visited[tmp].random = visited.get(tmp.random) #同样，visited[tmp.random]也会报错，非空报错
+            return clone
         return bfs(head)
 
 
 
-
+######################方法4 迭代
+###日后细究
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         visited = {}
