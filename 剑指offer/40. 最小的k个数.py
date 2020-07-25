738e05a55bcbbab9d410a02b20952dd22744e742
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-25 09:16:19 +0800

    Update 40. 最小的k个数.py

--- 剑指offer/40. 最小的k个数.py
+++ 剑指offer/40. 最小的k个数.py
@@ -1,4 +1,4 @@
-########################计数排序法
+########################方法1 计数排序法
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
         nums = [0] * 10000
@@ -15,3 +15,20 @@ class Solution:
 #时间复杂度：O(N+K)       
 #空间复杂度：O(K)???
 https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/bu-pai-xu-bu-long-guo-lu-qi-miao-sha-88-by-libins/
+
+    
+########################方法2 快排
+
+
+
+########################方法3 python sort()法
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        arr.sort()
+        return arr[:k]
+
+#时间复杂度：O(n*log n)，其中n是数组arr的长度。算法的时间复杂度即排序的时间复杂度。
+#空间复杂度：O(log n)，排序所需额外的空间复杂度为O(logn)。
+
+https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/zui-xiao-de-kge-shu-by-leetcode-solution/
+
