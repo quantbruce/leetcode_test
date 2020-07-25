07fe331e4dce89338766b6e90f2521e182841525
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-25 10:29:33 +0800

    Create 41. 数据流中的中位数(难).py

---
+++ 剑指offer/41. 数据流中的中位数(难).py
@@ -0,0 +1,32 @@
+###################方法1 Krahets 堆排序法
+######方法技巧性很强，基础要比较扎实
+
+
+from heapq import *
+
+class MedianFinder:
+    def __init__(self):
+        self.A = [] # 小顶堆，保存较大的一半
+        self.B = [] # 大顶堆，保存较小的一半
+
+    def addNum(self, num: int) -> None:
+        if len(self.A) != len(self.B):
+            heappush(self.A, num)
+            heappush(self.B, -heappop(self.A)) ###负号含义不理解，总容易混淆
+        else:
+            heappush(self.B, -num)
+            heappush(self.A, -heappop(self.B))
+
+    def findMedian(self) -> float:
+        return self.A[0] if len(self.A) != len(self.B) else (self.A[0] - self.B[0]) / 2.0
+
+#时间复杂度：O(logN)
+#空间复杂度：O(N)
+https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/mian-shi-ti-41-shu-ju-liu-zhong-de-zhong-wei-shu-y/
+
+
+
+
+
+
+
