0d78c6a06f1b20fc4412a38cb6807473159ba43a
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-25 10:57:20 +0800

    Update 42. 连续子数组的最大和.py

--- 剑指offer/42. 连续子数组的最大和.py
+++ 剑指offer/42. 连续子数组的最大和.py
@@ -1,4 +1,18 @@
-##动态规划法
+########### 方法1 普通动态规划法
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+        dp[0] = nums[0] 
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+        return max(dp)
+
+ 
+    
+    
+########### 方法2 动态规划法 压缩空间法
+### 由方法1演变而来
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
