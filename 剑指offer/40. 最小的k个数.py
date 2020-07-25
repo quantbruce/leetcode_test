9f9e26bc25441323ce3aa2b8e39d322acad6147d
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-25 09:11:07 +0800

    Update 40. 最小的k个数.py

--- 剑指offer/40. 最小的k个数.py
+++ 剑指offer/40. 最小的k个数.py
@@ -11,6 +11,7 @@ class Solution:
                 for _ in range(nums[i]):
                     output.append(i)
             i+=1
-        return output[:k]
-        
-        https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/bu-pai-xu-bu-long-guo-lu-qi-miao-sha-88-by-libins/
+        return output[:k] # 这个地方容易漏[:k]
+#时间复杂度：O(N+K)       
+#空间复杂度：O(K)???
+https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/bu-pai-xu-bu-long-guo-lu-qi-miao-sha-88-by-libins/
