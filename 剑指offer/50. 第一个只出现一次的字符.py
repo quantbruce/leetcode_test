3c74cbdd3eff91d68900eb220d220c811d6a2aa4
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-27 16:37:07 +0800

    Update 50. 第一个只出现一次的字符.py

--- 剑指offer/50. 第一个只出现一次的字符.py
+++ 剑指offer/50. 第一个只出现一次的字符.py
@@ -1,4 +1,4 @@
-########哈希表的使用
+########方法1 哈希表的使用
 
 class Solution:
     def firstUniqChar(self, s: str) -> str:
@@ -8,7 +8,22 @@ class Solution:
         for c in s:
             if dic[c] == 1: return c
         return " "
-        
-     https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/solution/mian-shi-ti-50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-3/
-        
+
+#时间复杂度：O(N)
+#空间复杂度：O(N)
+https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/solution/mian-shi-ti-50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-3/
+    
+
+##############方法2 对方法1的优化
+class Solution:
+    def firstUniqChar(self, s: str) -> str:
+        d = {}
+        for c in s:
+            d[c] = d.get(c, 0) + 1
+        for k,v in d.items():
+            if v==1: return k
+        return " "
+    
+#时间复杂度：O(N)
+#空间复杂度：O(N)
         
