6e49418850fa281dc10269ab67f02744c58ef6c4
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-27 21:55:13 +0800

    Update 55 - I. 二叉树的深度.py

--- 剑指offer/55 - I. 二叉树的深度.py
+++ 剑指offer/55 - I. 二叉树的深度.py
@@ -1,9 +1,13 @@
+##################方法1 递归法
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root: return 0  # 这个0 和下面的1组合就是妙用
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+#时间复杂度：O(N)
+#空间复杂度：O(N)
 
 
+#################方法2 迭代法
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root: return 0
@@ -16,6 +20,5 @@ class Solution:
             queue = tmp  # 这行代码放不放在for循环内部都是对的，但是放在外面效率高一些
             res += 1
         return res
-
 
 链接：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/solution/mian-shi-ti-55-i-er-cha-shu-de-shen-du-xian-xu-bia/
