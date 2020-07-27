0e65f72136770b6edabe198872f100f6bcc8a9e1
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-27 22:14:50 +0800

    Update 55 - I. 二叉树的深度.py

--- 剑指offer/55 - I. 二叉树的深度.py
+++ 剑指offer/55 - I. 二叉树的深度.py
@@ -14,11 +14,12 @@ class Solution:
         queue, res = [root], 0  ### queue=[], queue.append(root) 也可以写成这样。
         while queue:
             tmp = []
-            for node in queue:      # 这行代码写漏了，后面要加强记忆
+            for node in queue:      # 这行代码写漏了，后面要加强记忆. 这里倒出来的方式是采用遍历。不是通常的.pop()
                 if node.left: tmp.append(node.left)
                 if node.right: tmp.append(node.right)
             queue = tmp  # 这行代码放不放在for循环内部都是对的，但是放在外面效率高一些
             res += 1
         return res
-
+#时间复杂度：O(N)
+#空间复杂度：O(N) 最差情况下（当树平衡时），队列 queue 同时存储 N/2个节点。 ？？？？？？？？？？？？？？
 链接：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/solution/mian-shi-ti-55-i-er-cha-shu-de-shen-du-xian-xu-bia/
