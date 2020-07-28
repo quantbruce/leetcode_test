ea20542dd28701be2f4e214fffdea375704d1729
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-28 10:50:13 +0800

    Update 55 - II. 平衡二叉树(难).py

--- 剑指offer/55 - II. 平衡二叉树(难).py
+++ 剑指offer/55 - II. 平衡二叉树(难).py
@@ -1,5 +1,5 @@
-############## 方法1：后序遍历 + 剪枝 （从底至顶）
-
+############## 方法1：后序遍历 + 剪枝 （从底至顶） 最优
+### 见-1则不平衡，记套路
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
         def recur(root):
@@ -11,5 +11,19 @@ class Solution:
             return max(left, right)+1 if abs(left-right)<=1 else -1
         
         return recur(root)!=-1
+#时间复杂度：O(N)
+#空间复杂度：O(N)
+https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/solution/mian-shi-ti-55-ii-ping-heng-er-cha-shu-cong-di-zhi/
 
-    https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/solution/mian-shi-ti-55-ii-ping-heng-er-cha-shu-cong-di-zhi/
+
+    
+#############方法二：先序遍历 + 判断深度 （从顶至底）
+###容易想到，但是容易产生大量重复计算，低效
+
+
+
+#时间复杂度：O(N*log(N))
+#空间复杂度：O(N)
+https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/solution/mian-shi-ti-55-ii-ping-heng-er-cha-shu-cong-di-zhi/
+
+
