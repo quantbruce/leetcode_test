bb5c36b9713b08fdd0dfd1f4fa338a2fdef0d2c0
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-28 23:43:16 +0800

    Update 58 - II. 左旋转字符串.py

--- 剑指offer/58 - II. 左旋转字符串.py
+++ 剑指offer/58 - II. 左旋转字符串.py
@@ -18,9 +18,28 @@ class Solution:
         s_latter = ''.join(list(s)[n:])
         s_former = ''.join(list(s)[:n])
         return s_latter+s_former
+    
+########这样更精炼   
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        s_former = s[:n]
+        s_latter = s[n:]
+        return s_latter + s_former
+#时间复杂度：O(N) 仔细看解释
+#空间复杂度：O(N)
+https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/solution/mian-shi-ti-58-ii-zuo-xuan-zhuan-zi-fu-chuan-qie-p/
+    
+###########Krahets 取余法，容易理解
+#########Krahets关于这题的时间复杂度和空间复杂度以及测试的代码讲解的非常好，反复多看。直到融为一体
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = ""
+        for i in range(n, n + len(s)):
+            res += s[i%len(s)]
+        return res
+    
         
-        
-  #############除余法(变相循环，也是很巧妙)
+#############除余法(变相循环，也是很巧妙)
   class Solution:
     def reverseLeftWords(self, s: str, k: int) -> str:
         n = len(s)
@@ -29,3 +48,8 @@ class Solution:
             res+=s[i%n]
         return res
 https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/solution/chao-jian-dan-duo-chong-jie-fa-python3-by-azl39798/
+
+    
+    
+    
+    
