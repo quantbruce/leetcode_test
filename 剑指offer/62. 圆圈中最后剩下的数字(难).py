5aea972d406af37f5de6d2221d3295287073f499
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-29 15:17:25 +0800

    Update and rename 62. 圆圈中最后剩下的数字.py to 62. 圆圈中最后剩下的数字(难).py

--- 剑指offer/62. 圆圈中最后剩下的数字(难).py
+++ 剑指offer/62. 圆圈中最后剩下的数字(难).py
@@ -1,10 +1,12 @@
 #############约瑟夫环问题
+###观察找出的数学规律
 class Solution:
     def lastRemaining(self, n: int, m: int) -> int:
         final_index = 0
         for len_num in range(2, n+1): # i是数组长度
             final_index = (final_index+m)%len_num
         return final_index
-        
+#时间复杂度：O(N)
+#空间复杂度：O(1)
 https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/solution/javajie-jue-yue-se-fu-huan-wen-ti-gao-su-ni-wei-sh/
 https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/solution/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-by-lee/
