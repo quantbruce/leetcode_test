3aeefc4f1549184cffd3aa7da8640bf9abda5edb
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-29 11:11:28 +0800

    Update 59 - I. 滑动窗口的最大值(难).py

--- 剑指offer/59 - I. 滑动窗口的最大值(难).py
+++ 剑指offer/59 - I. 滑动窗口的最大值(难).py
@@ -33,14 +33,26 @@ class Solution:
             deque.append(i)
 
             #如果相聚超过窗口长度k，丢弃掉deque中首位index
-            while i-deque[0]>k-1:
+            while i-deque[0]>k-1:  # 这个while 可以改成if, 因为上面的deque.append(i)每一轮只加了一个进去
                 deque.pop(0)
             # 只要满足一个窗口的长度k，就向result中添加nums[deque[0]一个结果
             if i>=k-1:
                 result.append(nums[deque[0]]) #在整个过程中，始终保持deque[0]为最大值的index
         return result
     
+# 时间复杂度O(n)： 其中n为数组nums长度；线性遍历nums占用O(N)；每个元素最多仅入队和出队一次，因此单调队列deque占用O(2N) 。
+# 空间复杂度O(k)： 双端队列deque中最多同时存储k个元素（即窗口大小）。
+
+
 代码：    
 https://blog.csdn.net/weixin_43455338/article/details/105011557
 动画：
 https://www.cxyxiaowu.com/1141.html
+
+ 
+
+#############方法3 Krahets法
+
+https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/solution/mian-shi-ti-59-i-hua-dong-chuang-kou-de-zui-da-1-6/
+    
+    
