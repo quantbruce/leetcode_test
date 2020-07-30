f21cdfa077d1c9583488e3e344405ccb8e12a521
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-30 17:31:01 +0800

    Create 34(生).sql

---
+++ SQL/34(生).sql
@@ -0,0 +1,3 @@
+insert into actor('actor_id','first_name','last_name','last_update')
+values (1, 'PENELOPE', 'GUINESS', '2006-02-15 12:34:33'),
+       (2, 'NICK', 'WAHLBERG', '2006-02-15 12:34:33')
