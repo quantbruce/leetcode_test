33921d01470f7877340da846d1d6d56ea9d8f72d
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-30 16:40:26 +0800

    Create 27(难).sql

---
+++ SQL/27(难).sql
@@ -0,0 +1,7 @@
+select s2.emp_no, s2.from_date, (s2.salary-s1.salary) as salary_growth
+from salaries s1, salaries s2
+where salary_growth > 5000
+and s1.emp_no = s2.emp_no
+and (strftime('%Y', s2.to_date)-strftime('%Y', s1.to_date)=1 or
+    strftime('%Y', s2.from_date)-strftime('%Y', s1.from_date)=1)
+order by salary_growth desc
