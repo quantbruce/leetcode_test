9a355c0670406c444602f2cae0ffdb138a70d092
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-07-31 21:41:33 +0800

    Update 52.sql

--- SQL/52.sql
+++ SQL/52.sql
@@ -1,2 +1,7 @@
 select first_name from employees
 order by substr(first_name, -2) 
+
+
+substr(X,Y,Z) 或 substr(X,Y) 函数的使用。其中X是要截取的字符串。Y是字符串的起始位置（注意第一个字符的位置为1，而不为0），
+取值范围是±(1~length(X))，当Y等于length(X)时，则截取最后一个字符；当Y等于负整数-n时，则从倒数第n个字符处截取。
+Z是要截取字符串的长度，取值范围是正整数，若Z省略，则从Y处一直截取到字符串末尾；若Z大于剩下的字符串长度，也是截取到字符串末尾为止。
