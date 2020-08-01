81e6322f7089c61fe88bac769c2ef6dc933b1f04
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-01 20:19:31 +0800

    Create 54.sql

---
+++ SQL/54.sql
@@ -0,0 +1,4 @@
+select avg(salary) as avg_salary from salaries
+where to_date = '9999-01-01'
+and salary not in (select max(salary) from salaries where to_date='9999-01-01')
+and salary not in (select min(salary) from salaries where to_date='9999-01-01')
