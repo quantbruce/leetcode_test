c5a84a2f81b91fb7a37b90a49c93312cc40df4ab
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-06 10:34:48 +0800

    Create 177. 第N高的薪水.py

---
+++ SQL/Leetcode_SQL/177. 第N高的薪水.py
@@ -0,0 +1,19 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  declare p INT;
+  set p = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+        select 
+        (select distinct Salary 
+        from Employee
+        order by Salary desc
+        limit p,1)
+  );
+END
+
+https://leetcode-cn.com/problems/nth-highest-salary/solution/177-di-ngao-de-xin-shui-by-little_bird/
+
+
+#最优解答
+https://leetcode-cn.com/problems/nth-highest-salary/solution/mysql-zi-ding-yi-bian-liang-by-luanz/
