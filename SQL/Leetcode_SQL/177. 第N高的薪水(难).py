4a10f4383eae26f5e1ae59acbf7dbeba65d68ba5
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-06 10:46:38 +0800

    Update 177. 第N高的薪水(难).py

--- SQL/Leetcode_SQL/177. 第N高的薪水(难).py
+++ SQL/Leetcode_SQL/177. 第N高的薪水(难).py
@@ -1,6 +1,6 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  declare p INT; # declare关键字？？？？
+  declare p INT; # declare关键字？？？？, 总是忘记加封号
   set p = N - 1;
   RETURN (
       # Write your MySQL query statement below.
@@ -16,4 +16,19 @@ https://leetcode-cn.com/problems/nth-highest-salary/solution/177-di-ngao-de-xin-
 
 
 #最优解答
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+      SET N:=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT 
+            Salary
+      FROM 
+            Employee
+      GROUP BY 
+            Salary
+      ORDER BY 
+            Salary DESC
+      LIMIT N, 1
+  );
 https://leetcode-cn.com/problems/nth-highest-salary/solution/mysql-zi-ding-yi-bian-liang-by-luanz/
