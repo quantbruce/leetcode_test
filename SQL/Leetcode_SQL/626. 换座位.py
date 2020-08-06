c443e22fa5c79b57f8b3630fbd8b4bc5ce0876ab
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-06 17:06:00 +0800

    Update 626. 换座位.py

--- SQL/Leetcode_SQL/626. 换座位.py
+++ SQL/Leetcode_SQL/626. 换座位.py
@@ -11,4 +11,21 @@ order by id
 https://leetcode-cn.com/problems/exchange-seats/solution/jian-dan-yi-dong-xiao-lu-ji-bai-suo-you-by-fan-lu-/
 
 
+########官方解答法
+SELECT
+    (CASE
+        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND counts = id THEN id
+        ELSE id - 1
+    END) AS id,
+    student
+FROM
+    seat,
+    (SELECT
+        COUNT(*) AS counts
+    FROM
+        seat) AS seat_counts
+ORDER BY id ASC;
+
+# https://leetcode-cn.com/problems/exchange-seats/solution/huan-zuo-wei-by-leetcode/
 
