d2c980c2bf576628349044e82be8a9e69ee9cab3
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-09 14:25:19 +0800

    Update 68 - II. 二叉树的最近公共祖先(难).py

--- 剑指offer/68 - II. 二叉树的最近公共祖先(难).py
+++ 剑指offer/68 - II. 二叉树的最近公共祖先(难).py
@@ -38,7 +38,7 @@ class Solution:
         if not root or root==p or root==q:  #这几种情况合并的代码写的漂亮, 只有满足这行代码的root(其实也就是p,q), 才会被返回并记录left,和right节点值。
             return root
         left = self.lowestCommonAncestor(root.left, p, q) # left和right返回有4种组合，空、非空etc
-        right = self.lowestCommonAncestor(root.right, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q) # 体会：当只是要找到某个点时，递归一路往里面钻是不用把每一步的结果赋值给left,right等。只有有记录路径需求时才这样做
 
         if not left:  #就是应对上面left和right的几种组合而设置的
             return right
