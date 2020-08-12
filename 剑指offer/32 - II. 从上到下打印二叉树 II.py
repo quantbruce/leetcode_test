abb5bb14a36614f332acc3df3d863c2d6c181f1b
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-12 20:18:33 +0800

    Update 32 - II. 从上到下打印二叉树 II.py

--- 剑指offer/32 - II. 从上到下打印二叉树 II.py
+++ 剑指offer/32 - II. 从上到下打印二叉树 II.py
@@ -5,10 +5,10 @@ class Solution:
         queue.append(root)
         while queue:
             tmp = []
-            for _ in range(len(queue)):  # 每一层的元素有多少个，就循环多少次。这里是精华
+            for _ in range(len(queue)):  # 每一层的元素有多少个，就循环多少次。这里是精华, 这一行我漏写了
                 node = queue.popleft()
                 tmp.append(node.val)
-                if node.left: queue.append(node.left)
+                if node.left: queue.append(node.left) # 这里是queue.append(), 我错写成tmp.append()
                 if node.right: queue.append(node.right)
             res.append(tmp)
         return res
