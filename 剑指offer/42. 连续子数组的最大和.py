613d5a682465e6ed8d569d8aee6f0affda8abd56
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-17 15:26:59 +0800

    Update 42. 连续子数组的最大和.py

--- 剑指offer/42. 连续子数组的最大和.py
+++ 剑指offer/42. 连续子数组的最大和.py
@@ -7,8 +7,9 @@ class Solution:
         for i in range(1, len(nums)):
             dp[i] = max(dp[i-1]+nums[i], nums[i])
         return max(dp)
-
- 
+    
+#时间复杂度：O(N)
+#空间复杂度: O(N)
     
     
 ########### 方法2 动态规划法 压缩空间法
@@ -19,6 +20,8 @@ class Solution:
         for i in range(1, len(nums)):
             nums[i] += max(nums[i-1], 0)
         return max(nums)
-
+    
+#时间复杂度：O(N)
+#时间复杂度：O(1)
 https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution/mian-shi-ti-42-lian-xu-zi-shu-zu-de-zui-da-he-do-2/
 
