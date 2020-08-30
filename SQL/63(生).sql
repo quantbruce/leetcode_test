6f5a52cfd91d7ac9da53e183ed8b8a269ca2c634
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-08-30 17:09:37 +0800

    Create 63(生).sql

---
+++ SQL/63(生).sql
@@ -0,0 +1,5 @@
+select p1.id, p1.number, count(distinct p2.number) as rank
+from passing_number as p1, passing_number as p2
+where p1.number <= p2.number
+group by p1.id
+order by p1.number desc, p2.id
