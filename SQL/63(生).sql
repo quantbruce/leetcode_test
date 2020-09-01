d70ac63995acfce60cdee6eec6eddc1f13891360
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 17:13:19 +0800

    Update 63(生).sql

--- SQL/63(生).sql
+++ SQL/63(生).sql
@@ -3,3 +3,16 @@ from passing_number as p1, passing_number as p2
 where p1.number <= p2.number
 group by p1.id
 order by p1.number desc, p2.id
+
+
+-- My Method 较优
+select tt.number from
+(select number, count(*) as `num`
+from grade 
+group by number) as tt
+where tt.num >=3;
+
+-- 对我的方法的进一步改进， 最优
+select number from grade
+group by number
+having count(id) >= 3
