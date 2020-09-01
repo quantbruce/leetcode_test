3f975df43b589dbc17876ab6ef61f89cf8861979
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 13:24:02 +0800

    Update 35(生).sql

--- SQL/35(生).sql
+++ SQL/35(生).sql
@@ -1,3 +1,3 @@
-insert or ignore into actor('actor_id', 'first_name', 'last_name', 'last_update')
+insert or ignore into actor('actor_id', 'first_name', 'last_name', 'last_update') # 如果下面values()对应都给的对的话，上面的列名都可以省略掉
 values(3, 'ED', 'CHASE', '2006-02-15 12:34:33');
 
