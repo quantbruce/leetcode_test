c1ebc4248d2643718d2dc0f48f86b1b8cdcff74a
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 17:17:03 +0800

    Update 63(生).sql

--- SQL/63(生).sql
+++ SQL/63(生).sql
@@ -5,14 +5,3 @@ group by p1.id
 order by p1.number desc, p2.id
 
 
--- My Method 较优
-select tt.number from
-(select number, count(*) as `num`
-from grade 
-group by number) as tt
-where tt.num >=3;
-
--- 对我的方法的进一步改进， 最优
-select number from grade
-group by number
-having count(id) >= 3
