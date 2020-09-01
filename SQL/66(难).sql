ebe99bbf14ef5f11d055922b7c229f494f93725f
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 20:42:45 +0800

    Update 66(难).sql

--- SQL/66(难).sql
+++ SQL/66(难).sql
@@ -1,5 +1,5 @@
 select e.date, 
-round(sum(case e.type when 'completed' then 0 else 1 end)*1.0/count(e.type),3) as p    -- sum()和case()别弄混了
+round(sum(case e.type when 'completed' then 0 else 1 end)*1.0/count(e.type),3) as p    -- sum()和case()别弄混了, count()返回的是整数(个数) 
 from email e
 join user u1 on (e.send_id=u1.id and u1.is_blacklist=0)
 join user u2 on (e.receive_id=u2.id and u2.is_blacklist=0)
