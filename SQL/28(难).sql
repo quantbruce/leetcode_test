fd7424f5085554b318c65b9fefaf6e187fd81cc3
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 11:30:40 +0800

    Update 28(难).sql

--- SQL/28(难).sql
+++ SQL/28(难).sql
@@ -1,8 +1,9 @@
 select c.name, count(f.film_id) from
-(select category_id, count(film_id) from
-film_category group by category_id having count(category_id)>=5) as cc,
+(select category_id, count(film_id) from 
+film_category group by category_id having count(category_id)>=5) as cc, -- 不是找了之后再筛选>=5, 而是先筛选出>=5的这个群体，然后直接在里面再接着选
 film as f, category as c, film_category as fc
 where f.description like '%robot%'
 and f.film_id = fc.film_id
 and fc.category_id = c.category_id
-and cc.category_id = fc.category_id
+and cc.category_id = fc.category_id 
+
