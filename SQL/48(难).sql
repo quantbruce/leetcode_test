b78cf58ed551780ed20ecf0e95857eae289f9f32
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 15:48:18 +0800

    Update 48(难).sql

--- SQL/48(难).sql
+++ SQL/48(难).sql
@@ -1,6 +1,10 @@
+-- 错误写法
 update salaries set salary=salary*1.1 where emp_no in 
 (select s.emp_no from salaries s inner join emp_bonus eb
  on s.emp_no = eb.emp_no and s.to_date = '9999-01-01');
  
  
- 
+-- 正确写法
+update salaries set salary = salary * 1.1
+where emp_no in (select emp_no from emp_bonus)
+and to_date = '9999-01-01'
