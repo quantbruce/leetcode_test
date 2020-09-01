a7d556406fdb865ecd4260f7a1fc1659d5b56f09
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 20:31:39 +0800

    Update 68.sql

--- SQL/68.sql
+++ SQL/68.sql
@@ -4,3 +4,9 @@ join user u on l.user_id = u.id
 join client c on l.client_id = c.id
 group by l.user_id
 order by u.name
+
+-- 【心得】
+-- 分组之后，再取一条记录(最大或最小)， 此时选择group by + max or min
+-- 分组之后，再去前几条，或后几条记录时，有限考虑窗口函数！
+
+
