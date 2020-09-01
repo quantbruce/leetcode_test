1ee427c83f8b5ae45fb85095dbc35feff8b8a339
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 17:24:40 +0800

    Update 63(生).sql

--- SQL/63(生).sql
+++ SQL/63(生).sql
@@ -1,7 +1,7 @@
 select p1.id, p1.number, count(distinct p2.number) as rank
 from passing_number as p1, passing_number as p2
 where p1.number <= p2.number
-group by p1.id
+group by p1.id   -- 这一行总是容易漏写
 order by p1.number desc, p2.id
 
 
