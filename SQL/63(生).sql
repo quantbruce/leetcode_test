37d9dfa374a086ecf86cdf4d4efbe0576afe9704
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 17:21:38 +0800

    Update 63(生).sql

--- SQL/63(生).sql
+++ SQL/63(生).sql
@@ -5,3 +5,8 @@ group by p1.id
 order by p1.number desc, p2.id
 
 
+-- 窗口函数更酷！
+select id, number,
+dense_rank() over (order by number desc) as `rank`
+from passing_number 
+order by number  desc, id asc;
