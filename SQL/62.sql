245602cadfb67acdcbb7f5b6006a89bc6ce67442
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 17:17:24 +0800

    Update 62.sql

--- SQL/62.sql
+++ SQL/62.sql
@@ -3,3 +3,16 @@ select number from
 from grade
 group by number)
 where num >= 3;
+
+
+-- My Method 较优
+select tt.number from
+(select number, count(*) as `num`
+from grade 
+group by number) as tt
+where tt.num >=3;
+
+-- 对我的方法的进一步改进， 最优
+select number from grade
+group by number
+having count(id) >= 3
