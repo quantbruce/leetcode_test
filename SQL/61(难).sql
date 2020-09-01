3823c17966f87ab54d4518dd2a7f2f7de7663436
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 17:08:25 +0800

    Update 61(难).sql

--- SQL/61(难).sql
+++ SQL/61(难).sql
@@ -4,3 +4,16 @@ select e1.first_name from
         where e2.first_name>=e3.first_name)
         as rowid from employees as e2) as e1 
 where e1.rowid % 2 = 1
+
+
+
+-- 为什么我尝试用窗口函数却不对，navicat可以通过，而牛客网上却通过不了
+select tt.first_name
+from
+(select first_name,
+row_number() over (order by first_name) as row_idx
+from test_tab) as tt
+where tt.row_idx%2=1;
+
+
+
