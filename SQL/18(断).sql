3bd1fa66a641c5f6ea79be78c095466738e40a14
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 10:01:39 +0800

    Update 18(断).sql

--- SQL/18(断).sql
+++ SQL/18(断).sql
@@ -5,3 +5,15 @@ on e.emp_no = s.emp_no
 where s.to_date = '9999-01-01'
 and s.salary not in (
       select max(salary) from salaries)
+
+-- 这样写也是对的
+select s.emp_no, max(s.salary), e.last_name, e.first_name
+from salaries s
+join employees e
+on s.emp_no = e.emp_no
+where s.to_date = '9999-01-01'
+and s.salary <>
+(select max(salary)
+from salaries 
+where to_date = '9999-01-01')
+
