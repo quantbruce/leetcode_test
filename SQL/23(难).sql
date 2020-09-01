0faf66ea06b1aa7b10e330ef889d4671ae29ece3
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 10:26:38 +0800

    Update 23(难).sql

--- SQL/23(难).sql
+++ SQL/23(难).sql
@@ -4,3 +4,18 @@ where s1.to_date = '9999-01-01' and s2.to_date = '9999-01-01'
 and s1.salary <= s2.salary
 group by s1.emp_no
 order by s1.salary desc
+
+
+-- 写成窗口函数，简单明了的多
+select emp_no, salary,
+dense_rank() over (order by salary desc) as `rank`
+from salaries 
+where to_date = '9999-01-01' -- 这里写了order by emp_no 反而会报错
+
+-- 窗口函数也可以另写成这样
+ select emp_no,salary,
+dense_rank() over w as 'rank'
+from salaries
+where to_date='9999-01-01'
+WINDOW w as (order by salary desc)
+
