50460831811c11959dcbfb93be69ffb70341f085
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-01 22:02:45 +0800

    Update 03.插入排序.py

--- 十大经典排序算法/03.插入排序.py
+++ 十大经典排序算法/03.插入排序.py
@@ -3,7 +3,7 @@ def InsertionSort(nums):
     for i in range(len(nums)):
         preIndex = i - 1
         current = nums[i]
-        while preIndex >= 0 and nums[preIndex] > current:
+        while preIndex >= 0 and nums[preIndex] > current: # nums[preIndex] >= current，如果这样写，插入排序是否就变得不稳定了？
             nums[preIndex+1] = nums[preIndex]
             preIndex -= 1
         nums[preIndex+1] = current
