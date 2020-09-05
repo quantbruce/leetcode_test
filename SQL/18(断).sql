6865644b8a9ea1753859e67fcb92dfa082bc535d
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-05 10:05:03 +0800

    Update 18(断).sql

--- SQL/18(断).sql
+++ SQL/18(断).sql
@@ -17,3 +17,16 @@ and s.salary <>
 from salaries 
 where to_date = '9999-01-01')
 
+-- 更具有拓展性的框架
+select e.emp_no, s.salary, e.last_name, e.first_name
+from employees e join salaries s
+on e.emp_no = s.emp_no
+and s.to_date = '9999-01-01'
+and s.salary = (
+    select s1.salary
+    from salaries s1 join salaries s2
+    on s1.salary <= s2.salary
+    and s1.to_date = '9999-01-01' and s2.to_date = '9999-01-01'
+    group by s1.salary
+    having count(distinct s2.salary) = 2
+);
