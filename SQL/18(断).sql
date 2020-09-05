79d7d9b68dd19b414f0d9b8f757820b6dda4f9de
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-05 10:05:28 +0800

    Update 18(断).sql

--- SQL/18(断).sql
+++ SQL/18(断).sql
@@ -28,5 +28,5 @@ and s.salary = (
     on s1.salary <= s2.salary
     and s1.to_date = '9999-01-01' and s2.to_date = '9999-01-01'
     group by s1.salary
-    having count(distinct s2.salary) = 2
+    having count(distinct s2.salary) = 2 # 这里等于几，排名就写前几的
 );
