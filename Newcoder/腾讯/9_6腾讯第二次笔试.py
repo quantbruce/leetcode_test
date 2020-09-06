90981629c410a246e5ff77b4ff6dada060b6a597
quantbruce <48423247+quantbruce@users.noreply.github.com>
2020-09-06 22:11:16 +0800

    Create 9_6腾讯第二次笔试.py

---
+++ Newcoder/腾讯/9_6腾讯第二次笔试.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+def helper(n):
+    arr = []
+    for j in range(n):
+        tmp = list(map(int, input().strip().split(' ')))[:6]
+        arr.append(sorted(tmp))
+    for k in range(n - 1):
+        if sorted(arr[k]) == sorted(arr[k + 1]):
+            return "YES"
+        else:
+            return "NO"
+
+for _ in range(T):
+    n = int(input())
+    print(helper(n))
+    
+    
