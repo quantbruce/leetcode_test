90eb578440229ec251cac2130026c326796f1f35
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-03-01 21:05:36 +0800

    Update 213. 打家劫舍 II.py

--- 算法思想/动态规划/213. 打家劫舍 II.py
+++ 算法思想/动态规划/213. 打家劫舍 II.py
@@ -49,3 +49,42 @@ https://leetcode-cn.com/problems/house-robber-ii/solution/213-da-jia-jie-she-iid
 因为对于上述情况，即首尾都不取时，它的最大值肯定小于等于只去掉首或者只去掉尾的队列。
 即f（n1,n2,n3）<=f(n1,n2,n3,n4)
 """     
+
+
+### 方法2 动态规划
+# Inspired by 小抄
+
+"""
+执行用时：40 ms, 在所有 Python3 提交中击败了60.63%的用户
+内存消耗：14.6 MB, 在所有 Python3 提交中击败了76.80%的用户
+"""
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def robRange(nums, start, end):
+            dp_i, dp_i_1, dp_i_2 = 0, 0, 0
+            for i in range(end, start-1, -1):
+                dp_i = max(dp_i_1, nums[i] + dp_i_2)
+                dp_i_2 = dp_i_1
+                dp_i_1 = dp_i
+            return dp_i
+        n = len(nums)
+        if n==1: return nums[0]
+        return max(robRange(nums, 0, n-2), robRange(nums, 1, n-1))
+
+# 时间复杂度：O(N)
+# 空间复杂度：O(N)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
