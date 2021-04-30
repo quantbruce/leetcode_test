d8d88c5f2df776c12c83cb7c778458b71eba5c5b
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-05-01 00:13:06 +0800

    Create 12. 矩阵中的路径.cpp

---
+++ 剑指offer/C++_Version/12. 矩阵中的路径.cpp
@@ -0,0 +1,33 @@
+//方法1
+
+class Solution {
+
+public:
+    bool exist(vector<vector<char>>& board, string word)
+     {
+        for(int i=0; i < board.size(); i++)
+        {
+            for(int j=0; j < board[0].size(); j++)
+            {
+                if(dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    bool dfs(vector<vector<char>> &b, string &w, int i, int j, int k)
+    {
+        if(i<0 || i>=b.size() || j<0 || j>=b[0].size() || b[i][j]!=w[k])
+            return false;
+        if(k==w.size()-1)
+            return true;
+        char tmp = b[i][j];
+        b[i][j] = '#';
+        bool res = dfs(b,w,i+1,j,k+1) || dfs(b,w,i-1,j,k+1) || dfs(b,w,i,j+1,k+1) || dfs(b,w,i,j-1,k+1);
+        b[i][j] = tmp;
+        return res;
+    }
+
+};
