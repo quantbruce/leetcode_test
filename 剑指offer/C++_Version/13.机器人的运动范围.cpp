8d90f161995f17058db1cf4afcfeb4ad948038a5
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-05-01 00:01:25 +0800

    Update 13.机器人的运动范围.cpp

--- 剑指offer/C++_Version/13.机器人的运动范围.cpp
+++ 剑指offer/C++_Version/13.机器人的运动范围.cpp
@@ -23,3 +23,27 @@ private:
 
 
 // 方法2 BFS
+class Solution {
+
+public:
+    int movingCount(int m, int n, int k)
+    {  
+        vector<vector<bool>> visited(m, vector<bool>(n, 0));
+        queue<vector<int>> que;
+        int res=0;
+        que.push({0, 0, 0, 0});
+        while(que.size()>0)
+        {
+            vector<int> x = que.front();
+            que.pop();
+            int i=x[0], j=x[1], si=x[2], sj=x[3];
+            if(i<0 || i>=m || j<0 || j>=n || si+sj>k || visited[i][j])
+                continue;
+            visited[i][j] = true;
+            res++;
+            que.push({i+1, j, (i+1)%10 ? si+1 : si-8, sj});
+            que.push({i, j+1, si, (j+1)%10 ? sj+1 : sj-8});
+        }
+        return res;
+    }
+};
