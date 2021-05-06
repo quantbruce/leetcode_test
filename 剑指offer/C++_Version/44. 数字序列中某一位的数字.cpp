4a2998ae9ceeff6f06b36a9b2390642a1a270517
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-05-06 16:58:24 +0800

    Create 44. 数字序列中某一位的数字.cpp

---
+++ 剑指offer/C++_Version/44. 数字序列中某一位的数字.cpp
@@ -0,0 +1,19 @@
+//方法1
+
+class Solution {
+public:
+    int findNthDigit(int n) 
+    {
+        int digit=1;
+        long start=1, count=9;
+        while(n>count)
+        {
+            n -= count;
+            start *= 10;
+            digit += 1;
+            count = 9 * start * digit;
+        }
+        long num = start + (n-1) / digit;
+        return int(to_string(num)[(n-1)%digit])-'0'; // 它转换成字符的ASCII码作出数出字符串 
+    }
+};
