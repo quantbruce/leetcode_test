f61a2a3d1951b7c4d48bc63517f51d063050d95d
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-05-06 17:23:53 +0800

    Update 50. 第一个只出现一次的字符.cpp

--- 剑指offer/C++_Version/50. 第一个只出现一次的字符.cpp
+++ 剑指offer/C++_Version/50. 第一个只出现一次的字符.cpp
@@ -6,7 +6,7 @@ public:
     {
         unordered_map<char, int> dic;
         for(int i=0; i<s.size(); i++)
-            ++dic[s[i]];
+            ++dic[s[i]]; // 写成++dic[s[i]]也是对的，但是要好好理解下i++, 与++i的区别;
         for(char c : s)
             if(dic[c]==1)
                 return c;
