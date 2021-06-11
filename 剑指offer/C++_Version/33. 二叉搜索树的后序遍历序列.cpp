888e237340bc9ac90ab062d492060dc602f924ee
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-06-11 10:18:46 +0800

    Create 33. 二叉搜索树的后序遍历序列.cpp

---
+++ 剑指offer/C++_Version/33. 二叉搜索树的后序遍历序列.cpp
@@ -0,0 +1,26 @@
+//方法1
+class Solution {
+public:
+    bool verifyPostorder(vector<int>& postorder) 
+    {
+        return recur(postorder, 0, postorder.size()-1);
+    }
+
+private:
+    bool recur(vector<int>& postorder, int i, int j)
+    {
+        if(i>=j) return true;
+        int p = i;
+        while(postorder[p] < postorder[j]) 
+        {
+            p++;
+        }
+        int m = p;
+        while(postorder[p] > postorder[j])
+        {
+            p++;
+        }
+        return (p==j) && recur(postorder, i, m-1) && recur(postorder, m, j-1);
+    }
+};
+
