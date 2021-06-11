80374c7cdd6096988116d4c84b7018f9b1b1cf4f
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-06-11 10:27:42 +0800

    Create 43. 1～n 整数中 1 出现的次数.cpp

---
+++ 剑指offer/C++_Version/43. 1～n 整数中 1 出现的次数.cpp
@@ -0,0 +1,21 @@
+//方法1
+class Solution {
+public:
+    int countDigitOne(int n) 
+    {
+        long digit=1, res=0;
+        int high=n/10, cur=n%10, low=0;
+        while(high!=0 || cur!=0)
+        {
+            if(cur==0) res+=digit*high;
+            else if(cur==1) res+=digit*high+low+1;
+            else res+=(high+1)*digit;
+            low+=cur*digit;
+            cur=high%10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
+};
+
