c15579e0c6c17487871c970f33dbb5cca849f3b1
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-06-11 10:08:08 +0800

    Create 25. 合并两个排序的链表.cpp

---
+++ 剑指offer/C++_Version/25. 合并两个排序的链表.cpp
@@ -0,0 +1,36 @@
+//方法1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+        ListNode *cur = new ListNode(0);
+        ListNode *dum = cur;
+        while(l1 && l2)
+        {
+            if(l1->val <= l2->val)
+            {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if(l1)
+            cur->next = l1;
+        else
+            cur->next = l2;
+        return dum->next;
+    }
+};
