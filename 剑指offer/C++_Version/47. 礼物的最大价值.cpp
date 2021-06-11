8b9cc98b02e4481cff4f0254706175050d4853c5
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-06-11 10:30:06 +0800

    Create 47. 礼物的最大价值.cpp

---
+++ 剑指offer/C++_Version/47. 礼物的最大价值.cpp
@@ -0,0 +1,22 @@
+//方法1
+class Solution {
+public:
+    int maxValue(vector<vector<int>>& grid) 
+    {
+        for(int j=1; j<grid[0].size(); j++)
+            grid[0][j] += grid[0][j-1];
+
+        for(int i=1; i<grid.size(); i++)
+            grid[i][0] += grid[i-1][0];
+
+        for(int i=1; i<grid.size(); i++)
+        {
+            for(int j=1; j<grid[0].size(); j++)
+            {   
+                grid[i][j] += max(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+
+        return grid[grid.size()-1][grid[0].size()-1];
+    }
+};
